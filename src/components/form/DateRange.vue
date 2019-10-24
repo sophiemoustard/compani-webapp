7459b3a8b6f0a5e2803982d74851ff4640a49ec4
Sophie Moustard <sophie.moustard@gmail.com>
2019-10-24 17:28:08 +0200

    COM-766 - fix haserror

--- src/components/form/DateRange.vue
+++ src/components/form/DateRange.vue
@@ -2,7 +2,7 @@
   <div class='col-12'>
     <div v-if="caption" class="row justify-between">
       <p :class="['input-caption', { required: requiredField }]">{{ caption }}</p>
-      <q-icon v-if="hasError" name="error_outline" color="secondary" />
+      <q-icon v-if="error" name="error_outline" color="secondary" />
     </div>
     <q-field dense borderless :error="error" :error-message="errorLabel">
       <div :class="{ 'borders': !borderless }" class="date-container">
