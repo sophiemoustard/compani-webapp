b0fbe84027e9eff6c5f1b4395e6adfb159692761
Sophie Moustard <sophie.moustard@gmail.com>
2019-10-25 09:52:11 +0200

    Test build (#13)

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,5 @@
 module.exports = {
   presets: [
     '@quasar/babel-preset-app',
-    [
-      '@babel/preset-env', {
-        modules: false,
-        loose: false,
-        useBuiltIns: 'usage',
-      },
-    ],
-    [
-      '@babel/preset-stage-2', {
-        modules: false,
-        loose: false,
-        useBuiltIns: true,
-        decoratorsLegacy: true,
-      },
-    ],
   ],
-  // plugins: [
-  //   [
-  //     '@babel/transform-runtime', {
-  //       polyfill: false,
-  //       regenerator: false,
-  //     },
-  //   ],
-  // ],
-  // comments: false,
 };
