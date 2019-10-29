882a2b77fb74496208f51967b899443133961755
cmolle <chloe.molle@neuf.fr>
2019-10-29 15:13:48 +0100

    Com 780 (#30)
    * COM-780 beginning migration auxiliary infos (coach side)
    
    * COM-780 change style of the profile tabs
    
    * COM-780 changes from pr comments
    
    * COM-780 ordre alphabetique
    
    * COM-780 change from pr comments

---
+++ src/components/ProfileTabs.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="profile-tabs">
+    <q-tabs align="justify" color="transparent" text-color="primary" v-model="selectedTab">
+      <q-tab v-for="(tab, index) in tabsContent" :key="index" :label="tab.label"
+        :name="tab.name" :alert="alert(tab)"/>
+    </q-tabs>
+    <q-tab-panels v-model="selectedTab" class="no-border neutral-background" flat>
+      <q-tab-panel v-for="(tab, index) in tabsContent" :name="tab.name"  :key="index">
+        <component :is="tab.component" />
+      </q-tab-panel>
+    </q-tab-panels>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProfileTabs',
+  props: ['tabsContent', 'profileId'],
+  data () {
+    return {
+      selectedTab: 'info',
+    }
+  },
+  computed: {
+    notifications () {
+      return this.$store.getters['rh/getNotifications'];
+    },
+  },
+  methods: {
+    alert (tab) {
+      return tab.notification && this.notifications[tab.notification][this.profileId]
+        ? this.notifications[tab.notification][this.profileId]
+        : false
+    },
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+  .profile-tabs
+    /deep/ .q-tab--active
+      & > .q-tab__indicator
+        color: $primary
+    /deep/.q-tab__indicator
+      color: $light-grey
+      opacity: 1
+    & /deep/ .q-tabs
+      & div:nth-last-child(1)
+        margin-right: 0 !important
+      & .q-tab-panels .q-tab-panel
+        padding: 0
+        padding-top: 24px
+      & .q-tabs__content
+        &:not(.scrollable)
+          @media (min-width: 992px)
+            padding: 0
+        & .q-tab
+          flex: 1 1 0
+          padding-left: 0px
+          justify-content: start
+          margin-right: 24px
+          text-transform: none
+          & .q-tab__content
+            & .q-tab__label
+              color: $dark-grey
+              font-size: 24px
+            & .q-tab__alert
+              background: $secondary
+          &:before
+            background: none
+        & .q-tab--active
+          & .q-tab__content
+            & .q-tab__label
+              color: $primary
+              font-weight: 700
+</style>
