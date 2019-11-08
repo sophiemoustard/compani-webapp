fcff1d14c5ee0491ad9d396baff51dfb689345bb
Sophie Moustard <sophie.moustard@gmail.com>
2019-11-08 09:45:32 +0100

    Remove useless props

--- src/components/table/ResponsiveTable.vue
+++ src/components/table/ResponsiveTable.vue
@@ -1,7 +1,7 @@
 <template>
   <q-table :data="data" :columns="columns" :row-key="rowKey" :pagination="pagination" :visible-columns="visibleColumns"
     hide-bottom binary-state-sort @update:pagination="$emit('update:pagination', $event)" flat
-    class="table-responsive q-pa-sm">
+    class="table-responsive q-pa-sm" :rows-per-page-options="rowsPerPageOptions">
     <template v-slot:body="props">
       <slot name="body" :props="props">
         <q-tr :props="props">
@@ -22,12 +22,8 @@ export default {
     data: { type: Array, default: () => [] },
     columns: { type: Array, default: () => [] },
     pagination: { type: Object, default: () => ({ page: 1, rowsPerPage: 15 }) },
-    loading: { type: Boolean, default: false },
     rowKey: { type: String, default: 'name' },
     rowsPerPageOptions: { type: Array, default: () => [15, 25, 35] },
-    selection: { type: String, default: 'none' },
-    selected: { type: Array, default: () => [] },
-    separator: { type: String, default: 'horizontal' },
     visibleColumns: Array,
   },
 }
