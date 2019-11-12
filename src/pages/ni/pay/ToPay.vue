035e346181da94d9158d18638a9de2340aeab4c9
Sophie Moustard <sophie.moustard@gmail.com>
2019-11-12 14:30:45 +0100

    COM-834 - update title

--- src/pages/ni/pay/ToPay.vue
+++ src/pages/ni/pay/ToPay.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="neutral-background q-pb-xl">
-    <ni-title-header title="À payer">
+    <ni-title-header title="Paie mensuelle">
       <template slot="title">
         <q-btn round flat icon="save_alt" @click="exportToCSV" color="primary" style="margin-left: 5px"
           :disable="displayedDraftPay.length === 0" />
