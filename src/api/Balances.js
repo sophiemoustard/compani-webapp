8857f777bf23eec890401e9cf034ea60ed1147f9
cmolle <chloe.molle@neuf.fr>
2019-12-06 09:39:13 +0100

    COM-864 only one call to back (#103)

--- src/api/Balances.js
+++ src/api/Balances.js
@@ -5,4 +5,8 @@ export default {
     const balances = await alenviAxios.get(`${process.env.API_HOSTNAME}/balances`, { params });
     return balances.data.data.balances;
   },
+  async listWithDetails (params = null) {
+    const balances = await alenviAxios.get(`${process.env.API_HOSTNAME}/balances/details`, { params });
+    return balances.data.data;
+  },
 }
