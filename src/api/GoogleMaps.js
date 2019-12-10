bfab3e384763a58e5256d1c2a66d062c699f12ff
Chloe Molle <chloe.molle@neuf.fr>
2019-12-10 10:27:06 +0100

    COM-900 delete getdistancematrix route

--- src/api/GoogleMaps.js
+++
@@ -1,8 +0,0 @@
-import { alenviAxios } from './ressources/alenviAxios'
-
-export default {
-  async getDistanceMatrix (params) {
-    const direction = await alenviAxios.get(`${process.env.API_HOSTNAME}/google/maps/distancematrix`, { params });
-    return direction.data.data;
-  },
-}
