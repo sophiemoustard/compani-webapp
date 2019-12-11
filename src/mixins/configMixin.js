81745a0d8c9e205ba8ebab9960696754ce5d040b
Chloe Molle <chloe.molle@neuf.fr>
2019-12-11 16:20:42 +0100

    COM-902 config mixin

--- src/mixins/configMixin.js
+++ src/mixins/configMixin.js
@@ -3,23 +3,25 @@ import { NotifyNegative, NotifyPositive } from '../components/popup/notify';
 export const configMixin = {
   methods: {
     async deleteDocument (driveId, type, key) {
+      await this.$gdrive.removeFileById({ id: driveId });
+      const payload = {
+        [key]: {
+          templates: { [type]: { driveId: null, link: null } },
+        },
+      };
+      await this.$companies.updateById(this.company._id, payload);
+      this.refreshCompany();
+      NotifyPositive('Document supprimé');
+    },
+    async validateDocumentDeletion (driveId, type, key) {
       try {
         this.$q.dialog({
           title: 'Confirmation',
           message: 'Es-tu sûr(e) de vouloir supprimer ce document ?',
           ok: true,
           cancel: 'Annuler',
-        }).onOk(async () => {
-          await this.$gdrive.removeFileById({ id: driveId });
-          const payload = {
-            [key]: {
-              templates: { [type]: { driveId: null, link: null } },
-            },
-          };
-          await this.$companies.updateById(this.company._id, payload);
-          this.refreshCompany();
-          NotifyPositive('Document supprimé');
-        }).onCancel(() => NotifyPositive('Suppression annulée'));
+        }).onOk(() => this.deleteDocument(driveId, type, key))
+          .onCancel(() => NotifyPositive('Suppression annulée'));
       } catch (e) {
         console.error(e);
         NotifyNegative('Erreur lors de la suppression du document');
