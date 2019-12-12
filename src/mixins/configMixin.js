1624aa1645c9965a2120820564ab5916bac38623
Chloe Molle <chloe.molle@neuf.fr>
2019-12-12 09:21:31 +0100

    COM-902 fix after pr comment

--- src/mixins/configMixin.js
+++ src/mixins/configMixin.js
@@ -3,29 +3,29 @@ import { NotifyNegative, NotifyPositive } from '../components/popup/notify';
 export const configMixin = {
   methods: {
     async deleteDocument (driveId, type, key) {
-      await this.$gdrive.removeFileById({ id: driveId });
-      const payload = {
-        [key]: {
-          templates: { [type]: { driveId: null, link: null } },
-        },
-      };
-      await this.$companies.updateById(this.company._id, payload);
-      this.refreshCompany();
-      NotifyPositive('Document supprimé');
-    },
-    async validateDocumentDeletion (driveId, type, key) {
       try {
-        this.$q.dialog({
-          title: 'Confirmation',
-          message: 'Es-tu sûr(e) de vouloir supprimer ce document ?',
-          ok: true,
-          cancel: 'Annuler',
-        }).onOk(() => this.deleteDocument(driveId, type, key))
-          .onCancel(() => NotifyPositive('Suppression annulée'));
+        await this.$gdrive.removeFileById({ id: driveId });
+        const payload = {
+          [key]: {
+            templates: { [type]: { driveId: null, link: null } },
+          },
+        };
+        await this.$companies.updateById(this.company._id, payload);
+        this.refreshCompany();
+        NotifyPositive('Document supprimé');
       } catch (e) {
         console.error(e);
         NotifyNegative('Erreur lors de la suppression du document');
       }
+    },
+    async validateDocumentDeletion (driveId, type, key) {
+      this.$q.dialog({
+        title: 'Confirmation',
+        message: 'Es-tu sûr(e) de vouloir supprimer ce document ?',
+        ok: true,
+        cancel: 'Annuler',
+      }).onOk(() => this.deleteDocument(driveId, type, key))
+        .onCancel(() => NotifyPositive('Suppression annulée'));
     },
     documentUploaded () {
       NotifyPositive('Document envoyé');
