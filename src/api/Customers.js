325f3aa10be07a8058a67ffafcab2a2e482663ae
Sophie Moustard <sophie.moustard@gmail.com>
2019-12-16 16:38:58 +0100

    COM-922 - upadte returned value

--- src/api/Customers.js
+++ src/api/Customers.js
@@ -53,14 +53,14 @@ export default {
   },
   async getMandates (id) {
     const mandates = await alenviAxios.get(`${process.env.API_HOSTNAME}/customers/${id}/mandates`);
-    return mandates.data.data.mandates;
+    return mandates.data.data.customer.mandates;
   },
   async updateMandate (params, data) {
     return alenviAxios.put(`${process.env.API_HOSTNAME}/customers/${params._id}/mandates/${params.mandateId}`, data);
   },
   async getQuotes (id) {
     const quotes = await alenviAxios.get(`${process.env.API_HOSTNAME}/customers/${id}/quotes`);
-    return quotes.data.data.quotes;
+    return quotes.data.data.customer.quotes;
   },
   async addQuote (id, data) {
     return alenviAxios.post(`${process.env.API_HOSTNAME}/customers/${id}/quotes`, data);
