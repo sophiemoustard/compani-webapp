9c7928a419a0a64b09b341ac42582ee7b808d761
Sophie Moustard <sophie.moustard@gmail.com>
2019-12-20 14:10:08 +0100

    COM-926 - remove get payment route (#143)
    * COM-926 - remove get payment route
    
    * COM-926 - wording

--- src/api/Payments.js
+++ src/api/Payments.js
@@ -3,10 +3,6 @@ import { alenviAxios } from './ressources/alenviAxios'
 import { downloadFile } from '../helpers/downloadFile'
 
 export default {
-  async list (params) {
-    const payments = await alenviAxios.get(`${process.env.API_HOSTNAME}/payments`, { params });
-    return payments.data.data.payments;
-  },
   async create (data) {
     const payment = await alenviAxios.post(`${process.env.API_HOSTNAME}/payments`, data);
     return payment.data.data.payment;
