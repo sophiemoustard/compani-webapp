01486ef6f19db322fbd56424c4c57876d98b8eae
Chloe Molle <chloe.molle@neuf.fr>
2019-12-27 11:10:42 +0100

    COM-882 page customers fundings monitoring created

---
+++ src/pages/ni/customers/CustomersFundingsMonitoring.vue
@@ -0,0 +1,23 @@
+`<template>
+  <q-page class="neutral-background q-pb-xl">
+    <div class="title-padding">
+      <h4>Suivi des plans d'aide</h4>
+    </div>
+    <ni-large-table>
+    </ni-large-table>
+  </q-page>
+</template>
+
+<script>
+import LargeTable from '../../../components/table/LargeTable';
+
+export default {
+  name: 'CustomersFundingsMonitoring',
+  metaInfo: {
+    title: 'Suivi des plans d\'aide',
+  },
+  components: {
+    'ni-large-table': LargeTable,
+  },
+}
+</script>
