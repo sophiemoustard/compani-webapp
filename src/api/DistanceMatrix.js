90a1138d7cc69a3bb92ecaa34b5e5e5e57fb3683
jctrebalag <jctrebalag@gmail.com>
2020-01-17 11:37:55 +0100

    COM-974 Remove distances matrices call for planning

--- src/api/DistanceMatrix.js
+++
@@ -1,8 +0,0 @@
-import { alenviAxios } from './ressources/alenviAxios';
-
-export default {
-  async list (params = null) {
-    const distanceMatrices = await alenviAxios.get(`${process.env.API_HOSTNAME}/distancematrix`, { params });
-    return distanceMatrices.data.data.distanceMatrices;
-  },
-};
