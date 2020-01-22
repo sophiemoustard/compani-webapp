92730043822b610efa35fa78dc980d58f4096022
jctrebalag <jctrebalag@gmail.com>
2020-01-22 18:41:08 +0100

    COM-963 Refacto gauge

--- src/components/Gauge.vue
+++ src/components/Gauge.vue
@@ -1,12 +1,8 @@
 <template>
   <div id="wrapper" ref="wrapper">
     <svg id="meter">
-      <circle class="range" :r="radius" cx="50%" cy="50%" stroke="#930F59" :stroke-dasharray="strokes.first"
-        fill="none" />
-      <circle class="range" :r="radius" cx="50%" cy="50%" stroke="#F29400" :stroke-dasharray="strokes.second"
-        fill="none" />
-      <circle class="range" :r="radius" cx="50%" cy="50%" stroke="#D0D0D0" :stroke-dasharray="strokes.third"
-        fill="none" />
+      <circle v-for="(stroke, index) in strokes" :key="index" id="low" class="range" :r="radius" cx="50%" cy="50%"
+        :stroke="stroke.color" :stroke-width="stroke.width" :stroke-dasharray="computeStrokes(index)" fill="none" />
     </svg>
     <img id="meter_needle" src="~assets/gauge-needle.svg" alt="">
   </div>
@@ -17,19 +13,17 @@ export default {
   name: 'Gauge',
   props: {
     radius: { type: Number, default: 100 },
+    strokes: {
+      type: Array,
+      default: () => [{
+        color: String,
+        width: { type: Number, default: 30 },
+      }],
+    },
   },
   computed: {
     circumference () {
       return 2 * Math.PI * this.radius;
-    },
-    strokes () {
-      const semiCircumference = this.circumference / 2;
-
-      return {
-        first: `${semiCircumference}, ${this.circumference}`,
-        second: `${semiCircumference / 3 * 2}, ${this.circumference}`,
-        third: `${semiCircumference / 3}, ${this.circumference}`,
-      }
     },
   },
   mounted () {
@@ -38,8 +32,13 @@ export default {
   methods: {
     setWrapperDimensions () {
       const wrapperDimension = 2 * this.radius + 100;
-      this.$refs.wrapper.style.width = `${wrapperDimension}px`
-      this.$refs.wrapper.style.height = `${wrapperDimension}px`
+      this.$refs.wrapper.style.width = `${wrapperDimension}px`;
+      this.$refs.wrapper.style.height = `${wrapperDimension}px`;
+    },
+    computeStrokes (index) {
+      const semiCircumference = this.circumference / 2;
+      if (index === 0) return `${semiCircumference}, ${this.circumference}`;
+      return `${(this.strokes.length - index) * semiCircumference / this.strokes.length}, ${this.circumference}`;
     },
   },
 }
@@ -63,7 +62,4 @@ export default {
     top: 15%
     transform-origin: bottom center
     transform: rotate(340deg)
-
-  .range
-    stroke-width: 30
 </style>
