699ee2792859f46980af4f4d75804a5e8f893fda
Sophie Moustard <sophie.moustard@gmail.com>
2020-03-02 13:22:07 +0100

    COM-1028 - add list of companies

--- src/modules/vendor/pages/ni/users/companies/CompaniesDirectory.vue
+++ src/modules/vendor/pages/ni/users/companies/CompaniesDirectory.vue
@@ -1,17 +1,55 @@
 <template>
   <q-page class="neutral-background" padding>
     <ni-directory-header title="Répertoire structures" search-placeholder="Rechercher une structure" />
+    <q-table :data="companies" :columns="columns" row-key="name" binary-state-sort flat :loading="tableLoading"
+      :pagination.sync="pagination" class="people-list neutral-background" />
   </q-page>
 </template>
 
 <script>
+import Companies from '@api/Companies';
 import DirectoryHeader from '@components/DirectoryHeader';
+import { NotifyNegative } from '@components/popup/notify';
 
 export default {
   metaInfo: { title: 'Répertoire structures' },
   name: 'CompaniesDirectory',
   components: {
     'ni-directory-header': DirectoryHeader,
+  },
+  data () {
+    return {
+      companies: [],
+      tableLoading: false,
+      columns: [
+        {
+          name: 'name',
+          label: 'Nom',
+          align: 'left',
+          field: 'name',
+        },
+      ],
+      pagination: {
+        sortBy: 'startDate',
+        descending: true,
+        page: 1,
+        rowsPerPage: 15,
+      },
+    }
+  },
+  async mounted () {
+    await this.refreshCompanies();
+  },
+  methods: {
+    async refreshCompanies () {
+      try {
+        this.companies = await Companies.list();
+      } catch (e) {
+        console.error(e);
+        this.companies = [];
+        NotifyNegative('Erreur lors de la récupération des structures');
+      }
+    },
   },
 }
 </script>
