5907be01263e72d69f0f9ef20209a017a8cf61d6
jctrebalag <jctrebalag@gmail.com>
2020-03-05 23:18:54 +0100

    COM-1041 Set config env from quasar in cypress conf

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -16,6 +16,13 @@ const ctx = require('../../../quasar.conf');
 
 const { env } = ctx().build;
 
+const parseEnv = (env) => Object.keys(env).reduce((acc, key) => {
+  if (env[key]) {
+    acc[key] = JSON.parse(env[key]);
+  }
+  return acc;
+}, {});
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
@@ -37,7 +44,7 @@ module.exports = (on, config) => {
     },
   });
 
-  config.env = { ...env, NODE_ENV: 'test' };
+  config.env = { ...parseEnv(env) };
 
   return config;
 };
