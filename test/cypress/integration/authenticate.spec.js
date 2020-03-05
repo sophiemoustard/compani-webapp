5a32fe8bbd7aaf9a21e58f68ff4667fc51d5bb6f
jctrebalag <jctrebalag@gmail.com>
2020-03-05 23:18:05 +0100

    COM-1041 Add tests to authenticate e2e test

--- test/cypress/integration/authenticate.spec.js
+++ test/cypress/integration/authenticate.spec.js
@@ -1,10 +1,26 @@
 describe('Login page tests', () => {
   beforeEach(() => {
+    cy.request(`${Cypress.env('API_HOSTNAME')}/db/seed`);
     cy.visit('/login');
+  });
+
+  it('should display an error if credentials are wrong', () => {
+    cy.get('[data-cy=email]').type('admin@alenvi.io');
+    cy.get('[data-cy=password]').type('3236{enter}');
+    cy.get('.q-notification__message')
+      .should('be.visible')
+      .and('contain', 'Impossible de se connecter');
+    cy.url().should('include', '/login');
   })
 
   it('should visits login page and connects admin user', () => {
-    cy.get('[data-cy=email]').type('');
-    cy.get('[data-cy=password]').type('');
+    cy.get('[data-cy=email]').type('admin@alenvi.io');
+    cy.get('[data-cy=password]').type('123456');
+    cy.get('[data-cy=login]').click();
+    cy.url().should('include', '/ni/auxiliaries');
+    cy.getCookie('alenvi_token').should('exist');
+    cy.getCookie('alenvi_token_expires_in').should('exist');
+    cy.getCookie('refresh_token').should('exist');
+    cy.getCookie('user_id').should('exist');
   });
 });
