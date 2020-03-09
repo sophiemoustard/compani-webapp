5702d4fc092f8b1540bba4b764e0fb14ab3e43d9
jctrebalag <jctrebalag@gmail.com>
2020-03-09 11:23:57 +0100

    COM-1041 Fixes after PR comments

--- test/cypress/plugins/index.js
+++ test/cypress/plugins/index.js
@@ -3,18 +3,11 @@ const ctx = require('../../../quasar.conf');
 const { env } = ctx().build;
 
 const parseEnv = (env) => Object.keys(env).reduce((acc, key) => {
-  if (env[key]) {
-    acc[key] = JSON.parse(env[key]);
-  }
+  if (env[key]) acc[key] = JSON.parse(env[key]);
   return acc;
 }, {});
 
 module.exports = (on, config) => {
-  // `on` is used to hook into various events Cypress emits
-  // `config` is the resolved Cypress config
-  // console.log(config); // see what all is in here!
-
-  // Chrome:: Hack for shaking AUT. Cypress Issue: https://github.com/cypress-io/cypress/issues/1620
   on('before:browser:launch', (browser = {}, args) => {
     if (browser.name === 'chrome') {
       args.push('--disable-blink-features=RootLayerScrolling');
--- test/cypress/support/index.js
+++ test/cypress/support/index.js
@@ -4,8 +4,7 @@ const resizeObserverLoopErrRe = /^ResizeObserver loop limit exceeded/
 
 Cypress.on('uncaught:exception', (err) => {
   if (resizeObserverLoopErrRe.test(err.message)) {
-    // returning false here prevents Cypress from
-    // failing the test
+    // returning false here prevents Cypress from failing the test
     return false
   }
 })
