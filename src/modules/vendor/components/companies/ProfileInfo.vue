c6177573df3ad70612b17feecee3fdd5467d8fea
jctrebalag <jctrebalag@gmail.com>
2020-03-11 19:09:45 +0100

    COM-1061 Add user deletion

--- src/modules/vendor/components/companies/ProfileInfo.vue
+++ src/modules/vendor/components/companies/ProfileInfo.vue
@@ -17,7 +17,7 @@
                   <div class="row no-wrap table-actions">
                     <div class="row no-wrap table-actions">
                       <q-icon color="grey" name="edit" @click.native="openUserEditionModal(props.row)" />
-                      <!-- <q-icon color="grey" name="delete" @click.native="validateUserDeletion(col.value)" /> -->
+                      <q-icon color="grey" name="delete" @click.native="validateUserDeletion(col.value)" />
                     </div>
                   </div>
                 </template>
@@ -305,6 +305,25 @@ export default {
         console.error(e);
         NotifyNegative('Erreur lors de l\'édition de l\'utilisateur');
       }
+    },
+    async deleteUser (userId) {
+      try {
+        await Users.deleteById(userId);
+        await this.getUsers();
+        NotifyPositive('Utilisateur supprimé');
+      } catch (e) {
+        console.error(e);
+        NotifyNegative("Erreur lors de la suppression de l'utilisateur");
+      }
+    },
+    validateUserDeletion (userId) {
+      this.$q.dialog({
+        title: 'Confirmation',
+        message: 'Es-tu sûr(e) de vouloir supprimer cet utilisateur ?',
+        ok: true,
+        cancel: 'Annuler',
+      }).onOk(() => this.deleteUser(userId))
+        .onCancel(() => NotifyPositive('Suppression annulée'));
     },
   },
 }
