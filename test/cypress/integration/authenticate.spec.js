0670414fd3c95b767fb366e91ac9a92eeb4b4312
Chloe Molle <chloe.molle@neuf.fr>
2020-03-11 18:12:07 +0100

    COM-1068 all tests for logging

--- test/cypress/integration/authenticate.spec.js
+++ test/cypress/integration/authenticate.spec.js
@@ -23,4 +23,59 @@ describe('Login page tests', () => {
     cy.getCookie('refresh_token').should('exist');
     cy.getCookie('user_id').should('exist');
   });
+
+  it('should visits login page and connects coach user', () => {
+    cy.get('[data-cy=email]').type('coach@alenvi.io');
+    cy.get('[data-cy=password]').type('123456');
+    cy.get('[data-cy=login]').click();
+    cy.url().should('include', '/ni/auxiliaries');
+    cy.getCookie('alenvi_token').should('exist');
+    cy.getCookie('alenvi_token_expires_in').should('exist');
+    cy.getCookie('refresh_token').should('exist');
+    cy.getCookie('user_id').should('exist');
+  });
+
+  it('should visits login page and connects auxiliary user', () => {
+    cy.get('[data-cy=email]').type('auxiliary@alenvi.io');
+    cy.get('[data-cy=password]').type('123456');
+    cy.get('[data-cy=login]').click();
+    cy.url().should('include', '/auxiliaries/agenda');
+    cy.getCookie('alenvi_token').should('exist');
+    cy.getCookie('alenvi_token_expires_in').should('exist');
+    cy.getCookie('refresh_token').should('exist');
+    cy.getCookie('user_id').should('exist');
+  });
+
+  it('should visits login page and connects auxiliarywithoutcompany user', () => {
+    cy.get('[data-cy=email]').type('auxiliarywithoutcompany@alenvi.io');
+    cy.get('[data-cy=password]').type('123456');
+    cy.get('[data-cy=login]').click();
+    cy.url().should('include', '/account');
+    cy.getCookie('user_id').should('exist');
+    cy.getCookie('alenvi_token').should('exist');
+    cy.getCookie('alenvi_token_expires_in').should('exist');
+    cy.getCookie('refresh_token').should('exist');
+  });
+
+  it('should visits login page and connects planning-referent user', () => {
+    cy.get('[data-cy=email]').type('planning-referent@alenvi.io');
+    cy.get('[data-cy=password]').type('123456');
+    cy.get('[data-cy=login]').click();
+    cy.url().should('include', '/auxiliaries/agenda');
+    cy.getCookie('user_id').should('exist');
+    cy.getCookie('alenvi_token').should('exist');
+    cy.getCookie('alenvi_token_expires_in').should('exist');
+    cy.getCookie('refresh_token').should('exist');
+  });
+
+  it('should visits login page and connects helper user', () => {
+    cy.get('[data-cy=email]').type('helper@alenvi.io');
+    cy.get('[data-cy=password]').type('123456');
+    cy.get('[data-cy=login]').click();
+    cy.url().should('include', '/customers/agenda');
+    cy.getCookie('user_id').should('exist');
+    cy.getCookie('alenvi_token').should('exist');
+    cy.getCookie('alenvi_token_expires_in').should('exist');
+    cy.getCookie('refresh_token').should('exist');
+  });
 });
