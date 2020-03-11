69940fb5c99844d38b8ee57bb956d96434700725
jctrebalag <jctrebalag@gmail.com>
2020-03-11 16:48:04 +0100

    COM-1061 Add user modal creation

--- src/modules/vendor/components/companies/ProfileInfo.vue
+++ src/modules/vendor/components/companies/ProfileInfo.vue
@@ -27,25 +27,30 @@
           </template>
         </ni-responsive-table>
         <q-card-actions align="right">
-          <q-btn no-caps flat color="primary" icon="add" label="Ajouter un utilisateur" @click="userCreationModal = true" />
+          <q-btn no-caps flat color="primary" icon="add" label="Ajouter un utilisateur"
+            @click="userCreationModal = true" />
         </q-card-actions>
       </q-card>
     </div>
 
-    <ni-modal v-model="userCreationModal" @hide="resetUserForm">
+    <ni-modal v-model="userCreationModal" @hide="resetUserForm" @show="openUserCreationModal">
       <template slot="title">
         Ajouter un <span class="text-weight-bold">utilisateur</span>
       </template>
       <ni-input in-modal v-model="newUser.identity.firstname" caption="Prénom" />
-      <ni-input in-modal v-model="newUser.identity.lastname" caption="Nom" />
+      <ni-input in-modal v-model="newUser.identity.lastname" :error-label="REQUIRED_LABEL"
+        :error="$v.newUser.identity.lastname.$error" caption="Nom" @blur="$v.newUser.identity.lastname.$touch"
+        required-field />
       <ni-input in-modal v-model="newUser.local.email" :error="$v.newUser.local.email.$error" caption="Email"
         @blur="$v.newUser.local.email.$touch" :error-label="emailError($v.newUser)" required-field />
-      <ni-input in-modal v-model.trim="newUser.contact.phone" last :error="$v.newUser.contact.phone.$error"
+      <ni-input in-modal v-model.trim="newUser.contact.phone" :error="$v.newUser.contact.phone.$error"
         caption="Téléphone" @blur="$v.newUser.contact.phone.$touch" :error-label="phoneNbrError($v.newUser)" />
-      <!-- <template slot="footer"> -->
-        <!-- <q-btn no-caps class="full-width modal-btn" label="Ajouter un aidant" icon-right="add" color="primary"
-          :loading="loading" @click="submit" /> -->
-      <!-- </template> -->
+      <ni-select in-modal caption="Role" :options="roleOptions" v-model="newUser.role" :error-label="REQUIRED_LABEL"
+        :error="$v.newUser.role.$error" @blur="$v.newUser.role.$touch" last required-field />
+      <template slot="footer">
+        <q-btn no-caps class="full-width modal-btn" label="Ajouter un utilisateur" icon-right="add" color="primary"
+          :loading="loading" @click="createUser" :disable="$v.newUser.$anyError || !$v.newUser.$anyDirty"/>
+      </template>
     </ni-modal>
   </div>
 </template>
@@ -55,9 +60,12 @@ import { mapGetters } from 'vuex';
 import { required, email } from 'vuelidate/lib/validators';
 import get from 'lodash/get';
 import set from 'lodash/set';
+import pickBy from 'lodash/pickBy';
 import Companies from '@api/Companies';
 import Users from '@api/Users';
+import Roles from '@api/Roles';
 import Input from '@components/form/Input';
+import Select from '@components/form/Select';
 import Modal from '@components/modal/Modal';
 import ResponsiveTable from '@components/table/ResponsiveTable';
 import { NotifyNegative, NotifyWarning, NotifyPositive } from '@components/popup/notify';
@@ -73,11 +81,13 @@ export default {
   },
   components: {
     'ni-input': Input,
+    'ni-select': Select,
     'ni-modal': Modal,
     'ni-responsive-table': ResponsiveTable,
   },
   data () {
     return {
+      REQUIRED_LABEL,
       loading: false,
       userCreationModal: false,
       users: [],
@@ -134,6 +144,7 @@ export default {
         role: '',
         local: { email: '' },
       },
+      roles: [],
     };
   },
   validations () {
@@ -142,13 +153,18 @@ export default {
         name: { required },
       },
       newUser: {
+        identity: { lastname: { required } },
         local: { email: { required, email } },
         contact: { phone: { frPhoneNumber } },
+        role: { required },
       },
     };
   },
   computed: {
     ...mapGetters({ company: 'company/getCompany' }),
+    roleOptions () {
+      return this.roles.map(role => ({ label: coachRolesTranslation[role.name], value: role._id }));
+    },
   },
   async mounted () {
     if (!this.company) await this.refreshCompany();
@@ -193,6 +209,17 @@ export default {
         this.users = [];
       }
     },
+    async createUser () {
+      try {
+        await Users.create(pickBy(this.newUser));
+        this.userCreationModal = false;
+        await this.getUsers();
+        NotifyPositive('Utilisateur enregistré');
+      } catch (e) {
+        console.error(e);
+        NotifyNegative('Erreur lors de la création de l\'utilisateur');
+      }
+    },
     resetUserForm () {
       this.$v.newUser.$reset();
       this.newUSer = Object.assign({}, clear(this.newUser));
@@ -210,6 +237,19 @@ export default {
         return 'Numéro de téléphone non valide';
       }
       return '';
+    },
+    async getRoles () {
+      try {
+        this.roles = await Roles.list({ name: [CLIENT_ADMIN, COACH] });
+      } catch (e) {
+        console.error(e);
+        this.roles = [];
+      }
+    },
+    async openUserCreationModal () {
+      await this.getRoles();
+      const coachRole = this.roles.find(role => role.name === COACH);
+      if (coachRole) this.newUser.role = coachRole._id;
     },
   },
 }
