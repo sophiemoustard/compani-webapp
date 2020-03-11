b46cf638bae62c361c9a719528320ac5e9b30381
Sophie Moustard <sophie.moustard@gmail.com>
2020-03-11 08:59:29 +0100

    COM-1066 - fix quote download

--- src/store/main/getters.js
+++ src/store/main/getters.js
@@ -1,6 +1,11 @@
 export const toggleDrawer = (state) => {
   return state.toggleDrawer;
 };
+
 export const loggedUser = (state) => {
   return state.loggedUser;
+};
+
+export const company = (state) => {
+  return state.loggedUser.company;
 };
