3c10b953653c1f55242e52d32e7121c499cdc458
jctrebalag <jctrebalag@gmail.com>
2020-04-03 14:09:41 +0200

    COM-1043 Using router.replace instead of push

--- src/core/mixins/logInMixin.js
+++ src/core/mixins/logInMixin.js
@@ -16,7 +16,7 @@ export const logInMixin = {
       await this.$store.dispatch('main/getLoggedUser', this.$q.cookies.get('user_id'));
 
       if (this.$route.query.from) return this.$router.replace({ path: this.$route.query.from });
-      return this.$router.push('/').catch(e => {});
+      return this.$router.replace('/').catch(e => {});
     },
   },
 }
