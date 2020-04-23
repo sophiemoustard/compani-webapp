a582d3a311025dc66034b762d6c6134e7e39b64f
Corentin <corentin.martinot@gmail.com>
2020-04-23 21:08:54 +0200

    COM-1135 add refreshSortedCourses

--- src/modules/vendor/components/courses/CourseContainer.vue
+++ src/modules/vendor/components/courses/CourseContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="course-container">
     <div class="text-weight-bold q-mb-sm">{{ title }}</div>
-    <course-card class="q-mb-sm" v-for="(course, index) in courses" :key="index" :course="course"
+    <course-card class="q-mb-sm" v-for="(course, index) in courseSorted" :key="index" :course="course"
       @click="goToCourseProfile" />
   </div>
 </template>
@@ -18,40 +18,47 @@ export default {
   components: {
     'course-card': CourseCard,
   },
+  data () {
+    return {
+      courseSorted: [],
+    };
+  },
+  watch: {
+    courses () {
+      this.refreshCourseSorted();
+    },
+  },
   methods: {
     goToCourseProfile (course) {
       this.$router.push({ name: 'profile course info', params: { courseId: course._id } });
     },
-    rangeToNearestDate (course) {
-      if (this.courseSlotsCount === 0) return { label: 'Pas de date prévue', icon: 'flight_takeoff' };
-      const listDaySlots = Object.values(this.course.slots);
+    getRangeToNearestDate (course) {
+      if (course.slots.length === 0) return Number.MAX_SAFE_INTEGER;
+      const listDaySlots = Object.values(course.slots);
 
-      if (!listDaySlots.some((daySlot) => this.isDone(daySlot))) {
+      if (this.title === 'À venir') {
         const firstSlot = listDaySlots[0];
-        const rangeToNextDate = this.$moment(firstSlot[0].startDate).diff(this.$moment(0, 'HH'), 'd');
-        return {
-          label: `Commence dans ${rangeToNextDate} jour(s)`,
-          icon: 'flight_takeoff',
-        };
-      } else if (listDaySlots.every((daySlot) => this.isDone(daySlot))) {
+        const rangeToNextDate = this.$moment(firstSlot[0].startDate).diff(this.$moment(), 'd', true);
+        return rangeToNextDate;
+      } else if (this.title === 'Terminée(s)') {
         const lastSlot = listDaySlots[listDaySlots.length - 1];
-        const rangeToLastDate = this.$moment(0, 'HH').diff(this.$moment(lastSlot[0].startDate), 'd');
-        return {
-          label: `Dernière date il y a ${rangeToLastDate} jour(s) `,
-          icon: 'flight_land',
-        };
-      } else {
+        const rangeToLastDate = this.$moment().diff(this.$moment(lastSlot[0].startDate), 'd', true);
+        return rangeToLastDate;
+      } else if (this.title === 'En cours') {
         const nextSlot = listDaySlots.filter((daySlot) => !this.isDone(daySlot))[0];
-        const rangeToNextDate = this.$moment(nextSlot[0].startDate).diff(this.$moment(0, 'HH'), 'd');
-        if (rangeToNextDate === 0) {
-          return { label: 'Prochaine date aujourd’hui', icon: 'flight_takeoff' };
-        }
-        return {
-          label: `Prochaine date dans ${rangeToNextDate} jour(s)`,
-          icon: 'flight_takeoff',
-        };
+        const rangeToNextDate = this.$moment(nextSlot[0].startDate).diff(this.$moment(), 'd', true);
+        return rangeToNextDate;
       }
     },
+    isDone (daySlot) {
+      return this.$moment(daySlot[daySlot.length - 1].endDate).isBefore(this.$moment());
+    },
+    refreshCourseSorted () {
+      this.courseSorted = this.courses.sort((a, b) => this.getRangeToNearestDate(a) - this.getRangeToNearestDate(b));
+    },
+  },
+  mounted () {
+    this.refreshCourseSorted();
   },
 }
 </script>
