4e94e43406c819f59a6926f2737a22327535a3fa
Corentin <corentin.martinot@gmail.com>
2020-05-13 09:15:20 +0200

    COM-1185 first try to control favicon dynamically with VueMeta

--- src/modules/vendor/pages/ni/management/CoursesDirectory.vue
+++ src/modules/vendor/pages/ni/management/CoursesDirectory.vue
@@ -46,7 +46,18 @@ import CourseContainer from 'src/modules/vendor/components/courses/CourseContain
 import { courseMixin } from 'src/modules/vendor/mixins/courseMixin';
 
 export default {
-  metaInfo: { title: 'Catalogue' },
+  metaInfo: {
+    title: 'Catalogue',
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: 'https://res.cloudinary.com/alenvi/image/upload/v1546865717/images/business/Compani/' +
+          'compani_burgundy_32.png',
+      },
+    ],
+  },
   name: 'CoursesDirectory',
   mixins: [courseMixin],
   components: {
