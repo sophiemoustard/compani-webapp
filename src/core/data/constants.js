cd7c6a5d645f11f8e6965f92d1c70706750db421
Chloe Molle <chloe.molle@neuf.fr>
2020-05-14 16:41:36 +0200

    COM-1091 add hospitalized

--- src/core/data/constants.js
+++ src/core/data/constants.js
@@ -34,11 +34,13 @@ export const SURCHARGES = {
 export const UNKNOWN = 'unknown';
 export const HOME = 'home';
 export const NURSING_HOME = 'nursing_home';
+export const HOSPITALIZED = 'hospitalized';
 export const DECEASED = 'deceased';
 export const SITUATION_OPTIONS = [
   { label: 'Non renseigné', value: UNKNOWN },
   { label: 'Domicile', value: HOME },
   { label: 'EHPAD', value: NURSING_HOME },
+  { label: 'Hospitalisé', value: HOSPITALIZED },
   { label: 'Décédé', value: DECEASED },
 ];
 
