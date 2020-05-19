f4dcda8b6a21eff85c615f5c5a0a09fe91f80cec
Chloe Molle <chloe.molle@neuf.fr>
2020-05-19 12:21:49 +0200

    COm-1201 remove unused code in coursesdirectory for trainers

--- src/modules/vendor/pages/trainers/management/CoursesDirectory.vue
+++ src/modules/vendor/pages/trainers/management/CoursesDirectory.vue
@@ -6,13 +6,11 @@
 </template>
 
 <script>
-import { required, requiredIf } from 'vuelidate/lib/validators';
 import { mapState } from 'vuex';
 import groupBy from 'lodash/groupBy';
 import Courses from '@api/Courses';
 import TitleHeader from '@components/TitleHeader';
 import Trello from '@components/Trello';
-import { INTRA, COURSE_TYPES } from '@data/constants';
 import { courseMixin } from '@mixins/courseMixin';
 
 export default {
@@ -25,35 +23,11 @@ export default {
   },
   data () {
     return {
-      modalLoading: false,
-      newCourse: {
-        program: '',
-        company: '',
-        name: '',
-        type: INTRA,
-      },
-      courseCreationModal: false,
       coursesWithGroupedSlot: [],
-      programOptions: [],
-      companyOptions: [],
-      courseTypes: COURSE_TYPES,
-    }
-  },
-  validations () {
-    return {
-      newCourse: {
-        program: { required },
-        company: { required: requiredIf((item) => { return item.type === INTRA; }) },
-        name: { required },
-        type: { required },
-      },
     }
   },
   computed: {
     ...mapState('main', ['loggedUser']),
-    isIntraCourse () {
-      return this.newCourse.type === INTRA;
-    },
   },
   async created () {
     await this.refreshCourses();
