dc92b6e22352acb109fb5a7b289512074cfd2a99
Sophie Moustard <sophie.moustard@gmail.com>
2020-05-29 11:01:35 +0200

    COM-1173 - update background on client sside

---
+++ src/core/components/Banner.vue
@@ -0,0 +1,32 @@
+<template>
+  <q-banner :class="`full-width warning q-mb-md ${backgroundClass}`" dense>
+    <q-icon size="sm" name="warning" />
+    <div><slot name="message" /></div>
+  </q-banner>
+</template>
+
+<script>
+import { CLIENT, VENDOR } from '@data/constants';
+
+export default {
+  name: 'Banner',
+  props: {
+    icon: { type: String, default: 'icon' },
+  },
+  data () {
+    const interfaceType = /\/ad\//.test(this.$router.currentRoute.path) ? VENDOR : CLIENT;
+    return {
+      backgroundClass: interfaceType === CLIENT ? 'grey-background' : 'beige-background',
+    }
+  },
+}
+</script>
+
+<style lang="stylus" scoped>
+.q-banner
+  /deep/ .q-banner__content
+    display: flex;
+    align-items: center
+    .q-icon
+      margin-right: 5px
+</style>
