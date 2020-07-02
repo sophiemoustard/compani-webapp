eb5bbf0c0aa70e8f49ab9385db84a9ac070bc64f
Chloe Molle <chloe.molle@neuf.fr>
2020-07-02 15:24:57 +0200

    COM-1304 modale to add module + first display of modules

--- src/modules/vendor/components/programs/ProfileInfo.vue
+++ src/modules/vendor/components/programs/ProfileInfo.vue
@@ -11,6 +11,27 @@
           :error="$v.program.learningGoals.$error" />
       </div>
     </div>
+    <div class="q-mb-xl">
+      <p class="text-weight-bold">Modules</p>
+      <div v-for="(module, index) of program.modules" :key="index">
+          {{module.title}}
+      </div>
+      <q-btn class="fixed fab-custom" no-caps rounded color="primary" icon="add" label="Ajouter un module"
+        @click="moduleCreationModal = true" />
+    </div>
+
+    <!-- Module creation modal -->
+    <ni-modal v-model="moduleCreationModal" @hide="resetCreationModal">
+      <template slot="title">
+        Créer un nouveau <span class="text-weight-bold">module</span>
+      </template>
+      <ni-input in-modal v-model.trim="newModule.title" :error="$v.newModule.title.$error"
+        @blur="$v.newModule.title.$touch" required-field caption="Titre" />
+      <template slot="footer">
+        <q-btn no-caps class="full-width modal-btn" label="Créer le module" color="primary" :loading="modalLoading"
+          icon-right="add" @click="createModule" />
+      </template>
+    </ni-modal>
   </div>
 </template>
 
@@ -21,6 +42,7 @@ import get from 'lodash/get';
 import set from 'lodash/set';
 import Programs from '@api/Programs';
 import Input from '@components/form/Input';
+import Modal from '@components/modal/Modal';
 import { NotifyNegative, NotifyWarning, NotifyPositive } from '@components/popup/notify';
 
 export default {
@@ -30,15 +52,20 @@ export default {
   },
   components: {
     'ni-input': Input,
+    'ni-modal': Modal,
   },
   data () {
     return {
       tmpInput: '',
+      moduleCreationModal: false,
+      modalLoading: false,
+      newModule: { title: '' },
     }
   },
   validations () {
     return {
       program: { name: { required }, learningGoals: { required } },
+      newModule: { title: { required } },
     }
   },
   computed: {
@@ -78,6 +105,26 @@ export default {
       } finally {
         this.tmpInput = null;
       }
+    },
+    async createModule () {
+      try {
+        this.modalLoading = true;
+        this.$v.newModule.$touch();
+        if (this.$v.newModule.$error) return NotifyWarning('Champ(s) invalide(s)');
+        await Programs.addModule(this.profileId, this.newModule);
+        NotifyPositive('Module créé.');
+
+        await this.refreshProgram();
+        this.moduleCreationModal = false;
+      } catch (e) {
+        console.error(e);
+        NotifyNegative('Erreur lors de la création du module.');
+      } finally {
+        this.modalLoading = false;
+      }
+    },
+    resetCreationModal () {
+      this.newModule.title = '';
     },
   },
 }
