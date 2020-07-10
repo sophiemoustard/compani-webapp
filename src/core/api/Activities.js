46c2a429e6eb320378a4a16de1e75d436b6dd468
Corentin <corentin.martinot@gmail.com>
2020-07-10 11:27:49 +0200

    COM-1312 remove useless api object returns

--- src/core/api/Activities.js
+++ src/core/api/Activities.js
@@ -2,7 +2,6 @@ import { alenviAxios } from '@api/ressources/alenviAxios'
 
 export default {
   async updateById (activityId, payload) {
-    const activity = await alenviAxios.put(`${process.env.API_HOSTNAME}/activities/${activityId}`, payload);
-    return activity.data.data.activity;
+    await alenviAxios.put(`${process.env.API_HOSTNAME}/activities/${activityId}`, payload);
   },
 }
