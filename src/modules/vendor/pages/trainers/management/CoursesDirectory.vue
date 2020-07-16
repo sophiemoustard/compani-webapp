ecc3dcd8ab33a35687a2c6ceae47b6f2380ff523
Chloe Molle <chloe.molle@neuf.fr>
2020-07-16 11:35:08 +0200

    COM-1333 slots to plan creation

--- src/modules/vendor/pages/trainers/management/CoursesDirectory.vue
+++ src/modules/vendor/pages/trainers/management/CoursesDirectory.vue
@@ -49,7 +49,10 @@ export default {
         const courses = await Courses.list({ trainer: this.loggedUser._id });
         this.coursesWithGroupedSlot = courses.map(course => ({
           ...course,
-          slots: Object.values(groupBy(course.slots, s => this.$moment(s.startDate).format('DD/MM/YYYY'))),
+          slots: Object.values(groupBy(
+            course.slots.filter(slot => !!slot.startDate),
+            s => this.$moment(s.startDate).format('DD/MM/YYYY')
+          )),
         }));
       } catch (e) {
         console.error(e);
