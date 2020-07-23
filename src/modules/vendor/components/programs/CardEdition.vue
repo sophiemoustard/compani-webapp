d5d659afb9a2552ee50277497e5f24434bb3ca19
Chloe Molle <chloe.molle@neuf.fr>
2020-07-23 16:48:34 +0200

    COM-1309 add scroll area around card edition

--- src/modules/vendor/components/programs/CardEdition.vue
+++ src/modules/vendor/components/programs/CardEdition.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="q-mx-md card-edition">
-    <template v-if="card && Object.values(card).length">
-      <transition v-if="card.template === TRANSITION" class="q-mx-lg" :card="card" />
-      <title-text-media v-if="card.template === TITLE_TEXT_MEDIA" class="q-mx-lg" :card="card" />
-    </template>
+    <q-scroll-area :thumb-style="{ width: '6px', 'border-radius': '10px' }"
+      :content-style="{ display:'flex', 'flex-direction': 'column' }"
+      :content-active-style="{ display:'flex', 'flex-direction': 'column' }">
+        <div v-if="card && Object.values(card).length">
+          <transition v-if="card.template === TRANSITION" class="q-mx-lg" :card="card" />
+          <title-text-media v-if="card.template === TITLE_TEXT_MEDIA" class="q-mx-lg" :card="card" />
+        </div>
+    </q-scroll-area>
   </div>
 </template>
 
@@ -32,11 +36,14 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.q-scrollarea
+  height: 100%
+
 .card-edition
+  padding-top: 30px
   background-color: $middle-beige
   border-radius: 3px
   display: flex
   flex-direction: column
-  justify-content: center
   flex: 1
 </style>
