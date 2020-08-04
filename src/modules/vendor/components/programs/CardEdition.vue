3eefacd58405534d6eb4ae110b350a939dd323a6
Corentin Martinot <corentin.martinot@gmail.com>
2020-08-04 13:34:55 +0200

    add text-media edition

--- src/modules/vendor/components/programs/CardEdition.vue
+++ src/modules/vendor/components/programs/CardEdition.vue
@@ -7,6 +7,7 @@
           <transition v-if="card.template === TRANSITION" class="q-mx-lg" :card="card" />
           <title-text-media v-else-if="card.template === TITLE_TEXT_MEDIA" class="q-mx-lg" :card="card" />
           <title-text v-else-if="card.template === TITLE_TEXT" class="q-mx-lg" :card="card" />
+          <text-media v-else-if="card.template === TEXT_MEDIA" class="q-mx-lg" :card="card" />
         </div>
     </q-scroll-area>
   </div>
@@ -14,10 +15,11 @@
 
 <script>
 import { mapState } from 'vuex';
-import { TRANSITION, TITLE_TEXT_MEDIA, TITLE_TEXT } from '@data/constants';
+import { TRANSITION, TITLE_TEXT_MEDIA, TITLE_TEXT, TEXT_MEDIA } from '@data/constants';
 import Transition from 'src/modules/vendor/components/programs/templates/Transition';
 import TitleTextMedia from 'src/modules/vendor/components/programs/templates/TitleTextMedia';
 import TitleText from 'src/modules/vendor/components/programs/templates/TitleText';
+import TextMedia from 'src/modules/vendor/components/programs/templates/TextMedia'
 
 export default {
   name: 'CardEdition',
@@ -25,12 +27,14 @@ export default {
     transition: Transition,
     'title-text-media': TitleTextMedia,
     'title-text': TitleText,
+    'text-media': TextMedia,
   },
   data () {
     return {
       TRANSITION,
       TITLE_TEXT_MEDIA,
       TITLE_TEXT,
+      TEXT_MEDIA,
     };
   },
   computed: {
