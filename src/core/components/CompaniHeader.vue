ec60c518f8dfd4268c35628d1c713fb4bbe21de8
Sophie Moustard <sophie.moustard@gmail.com>
2020-08-17 11:57:20 +0200

    COM-1402 - remove functional

--- src/core/components/CompaniHeader.vue
+++ src/core/components/CompaniHeader.vue
@@ -1,4 +1,4 @@
-<template functional>
+<template>
   <div class="row justify-between signup-header-padding bg-white items-center col-12">
     <img style="height: 30px" :src="PINK_LOGO" alt="Logo Compani">
     <p class="no-margin" style="font-size: 0.8rem; text-align: right">Espace Alenvi</p>
