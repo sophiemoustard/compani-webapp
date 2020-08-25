5aab47e8c159383688ef4e9a4ad87618cc054416
Chloe Molle <chloe.molle@neuf.fr>
2020-08-25 14:58:31 +0200

    COM-1409 fix warning error when there is only one tag with error

--- src/core/helpers/vuelidateCustomVal.js
+++ src/core/helpers/vuelidateCustomVal.js
@@ -131,7 +131,8 @@ export const validTagsCount = (value) => {
   if (!value) return true;
   const { gapAcc } = parseTagCode(value);
 
-  return gapAcc.length === 1 || (gapAcc.length === 2 && validTagging(value));
+  return (gapAcc.length === 0 && !validTagging(value)) || gapAcc.length === 1 ||
+    (gapAcc.length === 2 && validTagging(value));
 };
 
 export const min2Answers = value => value.filter(a => !!a.label).length > 1;
