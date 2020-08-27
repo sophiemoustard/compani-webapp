30a3cff2420d13d59e556f623e7ad4f62940359d
Chloe Molle <chloe.molle@neuf.fr>
2020-08-27 15:43:19 +0200

    COM-1426 can detach steps from subprogram

--- src/core/api/SubPrograms.js
+++ src/core/api/SubPrograms.js
@@ -7,4 +7,7 @@ export default {
   async addStep (subProgramId, payload) {
     await alenviAxios.post(`${process.env.API_HOSTNAME}/subprograms/${subProgramId}/steps`, payload);
   },
+  async detachStep (subProgramId, stepId) {
+    await alenviAxios.delete(`${process.env.API_HOSTNAME}/subprograms/${subProgramId}/steps/${stepId}`);
+  },
 };
