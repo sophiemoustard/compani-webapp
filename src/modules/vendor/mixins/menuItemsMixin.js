8bce054c979897f8d9140ad703db5f7cd332d023
Félix <felix@alenvi.io>
2020-09-23 10:55:16 +0200

    COM-1511 sorted by date

--- src/modules/vendor/mixins/menuItemsMixin.js
+++ src/modules/vendor/mixins/menuItemsMixin.js
@@ -16,7 +16,7 @@ export const menuItemsMixin = {
           label: 'Gestion',
           children: [
             { name: 'ni management blended courses', icon: 'mdi-teach', label: 'Formations mixtes' },
-            { name: 'ni management elearning courses', icon: 'mdi-teach', label: 'Formations eLearning' },
+            { name: 'ni management elearning courses', icon: 'reorder', label: 'Formations eLearning' },
           ],
         },
         {
