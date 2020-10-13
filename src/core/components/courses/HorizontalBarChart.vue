89b2123e7be49f3aa4ddb44a0416f625add78140
Félix <felix@alenvi.io>
2020-10-13 15:37:10 +0200

    COM-1516 create HorizontalBarChart

---
+++ src/core/components/courses/HorizontalBarChart.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <q-linear-progress size="48px" :value="0.5" rounded class="q-mt-sm bar">
+      <div class="bar-label">
+        <div>Un reseau wifi</div>
+        <div>12 réponses</div>
+      </div>
+    </q-linear-progress>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HorizontalBarChart',
+  data () {
+    return {
+    };
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.bar
+  color: $middle-beige
+.bar-label
+  font-size: 14px
+  position: absolute
+  color: black
+  display: flex
+  justify-content: space-between
+  width: 100%
+  align-items: center
+  height: 100%
+  padding: 0 24px
+</style>
--- src/core/components/courses/ProfileTraineeFollowUp.vue
+++ src/core/components/courses/ProfileTraineeFollowUp.vue
@@ -1,9 +1,16 @@
 <template>
-  <div />
+  <div>
+    <horizontal-bar-chart />
+  </div>
 </template>
 
 <script>
+import HorizontalBarChart from '@components/courses/HorizontalBarChart';
+
 export default {
   name: 'ProfileTraineeFollowUp',
+  components: {
+    'horizontal-bar-chart': HorizontalBarChart,
+  },
 };
 </script>
