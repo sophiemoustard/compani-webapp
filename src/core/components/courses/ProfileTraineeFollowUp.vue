86bdf8e30476883653d9476875c3a3e85dccf4cd
Sophie Moustard <sophie.moustard@gmail.com>
2020-10-14 15:35:52 +0200

    COM-1516 list steps

--- src/core/components/courses/ProfileTraineeFollowUp.vue
+++ src/core/components/courses/ProfileTraineeFollowUp.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
+  <div v-if="followUp.subProgram">
+    <div v-for="(step, stepIndex) of followUp.subProgram.steps" :key="stepIndex" class="q-mb-xl">
+      <div class="text-weight-bold">{{ stepIndex + 1 }} - {{ step.name }}</div>
+    </div>
     <horizontal-bar-chart />
   </div>
 </template>
@@ -17,14 +20,20 @@ export default {
   props: {
     profileId: { type: String, required: true },
   },
+  data () {
+    return {
+      followUp: {},
+    };
+  },
   async created () {
     await this.refreshFollowUp();
   },
   methods: {
     async refreshFollowUp () {
       try {
-        await Courses.getFollowUp(this.profileId);
+        this.followUp = await Courses.getFollowUp(this.profileId);
       } catch (e) {
+        this.followUp = {};
         NotifyNegative('Erreur lors de la récupération du suivi des stagiaires.');
         console.error(e);
       }
