3ebbf160109f1ec3b612e23b50957392a53550b3
Sophie Moustard <sophie.moustard@gmail.com>
2020-10-14 15:21:59 +0200

    COM-1516 add GET route

--- src/core/components/courses/ProfileTraineeFollowUp.vue
+++ src/core/components/courses/ProfileTraineeFollowUp.vue
@@ -5,12 +5,30 @@
 </template>
 
 <script>
+import { NotifyNegative } from '@components/popup/notify';
 import HorizontalBarChart from '@components/courses/HorizontalBarChart';
+import Courses from '@api/courses';
 
 export default {
   name: 'ProfileTraineeFollowUp',
   components: {
     'horizontal-bar-chart': HorizontalBarChart,
+  },
+  props: {
+    profileId: { type: String, required: true },
+  },
+  async created () {
+    await this.refreshFollowUp();
+  },
+  methods: {
+    async refreshFollowUp () {
+      try {
+        await Courses.getFollowUp(this.profileId);
+      } catch (e) {
+        NotifyNegative('Erreur lors de la récupération du suivi des stagiaires.');
+        console.error(e);
+      }
+    },
   },
 };
 </script>
