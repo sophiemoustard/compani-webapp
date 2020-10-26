6837996ca8092e9ce9d5da31a0ff9d369eaa1ea7
Félix <felix@alenvi.io>
2020-10-26 16:37:42 +0100

    COM-1610 refacto SurveyChart

--- src/core/components/courses/SurveyChart.vue
+++ src/core/components/courses/SurveyChart.vue
@@ -1,15 +1,24 @@
 <template>
-  <horizontal-bar-chart :title="card.question" :subtitle="subtitle" :lines="lines" />
+  <q-card class="card" flat>
+    <div class="text-weight-bold">{{ card.question }}</div>
+    <div class="q-mb-lg subtitle">{{ subtitle }}</div>
+    <div v-for="(line, index) in lines" :key="index" class="q-mt-sm bar-container">
+      <div class="q-mr-sm percentage">{{ formatPercentage(line.percentage) }}</div>
+      <q-linear-progress size="40px" :value="line.percentage" rounded class="bar">
+        <div class="bar-label">
+          <div>{{ line.title }}</div>
+          <div><span class="text-weight-bold">{{ line.total }}</span> réponses</div>
+        </div>
+      </q-linear-progress>
+    </div>
+  </q-card>
 </template>
 
 <script>
-import HorizontalBarChart from '@components/courses/HorizontalBarChart';
+import { roundFrenchPercentage } from '@helpers/utils';
 
 export default {
   name: 'SurveyChart',
-  components: {
-    'horizontal-bar-chart': HorizontalBarChart,
-  },
   props: {
     card: { type: Object, default: () => ({}) },
   },
@@ -25,5 +34,45 @@ export default {
       });
     },
   },
+  methods: {
+    formatPercentage (number) {
+      return roundFrenchPercentage(number * 100, 1);
+    },
+  },
 };
 </script>
+
+<style lang="stylus" scoped>
+.card
+  padding: 16px 32px
+
+.subtitle
+  color: $dark-grey
+  font-size: 14px
+
+.percentage
+  text-align: center
+  width: 56px
+
+.bar-container
+  display: flex
+  align-items: center
+
+.bar
+  color: $middle-beige
+  background-color: $neutral-beige
+
+/deep/ .q-linear-progress__track
+  opacity: 0
+
+.bar-label
+  font-size: 14px
+  position: absolute
+  color: black
+  display: flex
+  justify-content: space-between
+  width: 100%
+  align-items: center
+  height: 100%
+  padding: 0 24px
+</style>
