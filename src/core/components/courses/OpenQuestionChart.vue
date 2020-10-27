b4535a6b7f419168e5572c3ebe9ad1c231893186
Félix <felix@alenvi.io>
2020-10-27 16:52:33 +0100

    COM-1609 back to q-card

--- src/core/components/courses/OpenQuestionChart.vue
+++ src/core/components/courses/OpenQuestionChart.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="card rounded-borders">
+  <q-card class="card" flat>
     <div class="text-weight-bold">{{ card.question }}</div>
     <div class="q-mb-lg subtitle">{{ subtitle }}</div>
-    <div v-for="(answer, index) in card.answers" :key="index"
-      class="q-mt-sm answer-container rounded-borders">
-      <div class="q-ma-sm" style="display:flex">{{ answer }}</div>
+    <div v-for="(answer, index) in card.answers" :key="index" class="q-mt-sm answer-container">
+      <div class="q-ma-sm">{{ answer }}</div>
     </div>
-  </div>
+  </q-card>
 </template>
 
 <script>
@@ -26,11 +25,11 @@ export default {
 <style lang="stylus" scoped>
 .card
   padding: 16px 32px
-  background-color: white
 .subtitle
   color: $dark-grey
   font-size: 14px
 .answer-container
+  border-radius: 3px !important
   font-size: 14px
   display: flex
   background-color: $neutral-beige
