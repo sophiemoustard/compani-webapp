11107dbca42dee2ad50d6510c8b699d0a5c1bcbf
Félix <felix@alenvi.io>
2020-10-30 09:37:32 +0100

    COM-1606 relative chart width

--- src/core/components/courses/SurveyChart.vue
+++ src/core/components/courses/SurveyChart.vue
@@ -2,18 +2,20 @@
   <q-card class="card" flat>
     <div class="text-weight-bold">{{ card.question }}</div>
     <div class="q-mb-lg subtitle">{{ subtitle }}</div>
-    <div class="bar-container">
-      <div v-for="(line, index) in lines" :key="index" class="q-my-sm">
-        <div class="bar rounded-borders">
-          <div class="q-mt-sm bar-label">{{ index + 1 }}</div>
-          <div class="bar-fill rounded-borders" :style="`height: ${line.percentage * 100}%`" />
+    <div class="chart">
+      <div class="bar-container">
+        <div v-for="(line, index) in lines" :key="index">
+          <div class="bar rounded-borders">
+            <div class="bar-fill rounded-borders" :style="`height: ${line.percentage * 100}%`" />
+            <div class="bar-label">{{ index + 1 }}</div>
+          </div>
+          <div class="percentage">{{ formatPercentage(line.percentage) }}</div>
         </div>
-        <div class="percentage">{{ formatPercentage(line.percentage) }}</div>
       </div>
-    </div>
-    <div class="chart-footer">
-      <div>{{ this.card.label.left }}</div>
-      <div>{{ this.card.label.right }}</div>
+      <div class="chart-footer">
+        <div>{{ this.card.label.left }}</div>
+        <div>{{ this.card.label.right }}</div>
+      </div>
     </div>
   </q-card>
 </template>
@@ -60,33 +62,33 @@ export default {
 .bar-container
   display: flex
   justify-content: space-evenly
-  width: 304px
+  width: 50%
+
+.chart
+  display: flex
+  flex-direction: column
 
 .bar
   position: relative
   color: $middle-beige
   background-color: $neutral-beige
   width: 48px
-  height: 344px
-  display: flex
-  align-content: space-between
-  flex-wrap: wrap
+  height: 320px
 
 .chart-footer
   display: flex
   justify-content: space-between
-  width: 304px
+  width: 50%
 
 .bar-fill
   position: absolute
   bottom: 0px
   background-color: $middle-beige
-  width: 48px
+  width: 100%
 
 .bar-label
   z-index: 2
   font-size: 14px
   color: black
   text-align: center
-  width: 100%
 </style>
