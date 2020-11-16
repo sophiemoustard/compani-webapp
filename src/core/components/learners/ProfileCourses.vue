369f6cfd13973c07b23d8a6560b4070912378278
Félix <felix@alenvi.io>
2020-11-16 16:14:22 +0100

    COM-1630 add type column

--- src/core/components/learners/ProfileCourses.vue
+++ src/core/components/learners/ProfileCourses.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="q-mb-xl">
     <p class="text-weight-bold q-mb-none">Formations suivies</p>
-    <ni-table-list :data="orderedCourses" :columns="columns" @go-to="goToBlendedCourseProfileAdmin" />
+    <ni-table-list :data="orderedCourses" :columns="columns" @go-to="goToBlendedCourseProfileAdmin"
+      :pagination.sync="pagination" />
   </div>
 </template>
 
@@ -11,6 +12,7 @@ import Courses from '@api/Courses';
 import TableList from '@components/table/TableList';
 import { FORTHCOMING, IN_PROGRESS, COMPLETED, BLENDED } from '@data/constants';
 import { userMixin } from '@mixins/userMixin';
+import { sortStrings } from '@helpers/utils';
 import { courseFiltersMixin } from '@mixins/courseFiltersMixin';
 import { courseTimelineMixin } from '@mixins/courseTimeline';
 
@@ -28,15 +30,32 @@ export default {
         [IN_PROGRESS]: 'En cours',
         [COMPLETED]: 'Terminée',
       },
+      pagination: {
+        sortBy: 'name',
+        descending: false,
+        page: 1,
+        rowsPerPage: 15,
+      },
       columns: [
         {
           name: 'name',
           label: 'Nom',
           field: row => row,
           align: 'left',
-          sortable: false,
+          sortable: true,
           format: value => value.subProgram.program.name + (value.misc ? ` - ${value.misc}` : ''),
+          sort: (a, b) => sortStrings(a.subProgram.program.name, b.subProgram.program.name),
           style: 'min-width: 200px; width: 65%',
+        },
+        {
+          name: 'type',
+          label: 'Type',
+          field: 'format',
+          align: 'left',
+          sortable: true,
+          format: value => ((value === BLENDED) ? 'Formation mixte' : 'Formation eLearning'),
+          sort: (a, b) => sortStrings(a, b),
+          style: 'min-width: 110px; width: 35%',
         },
         {
           name: 'status',
@@ -57,7 +76,7 @@ export default {
     },
   },
   async created () {
-    const courses = await Courses.list({ trainees: this.userProfile._id, format: BLENDED });
+    const courses = await Courses.list({ trainees: this.userProfile._id });
     this.courses = this.groupByCourses(courses);
   },
   methods: {
