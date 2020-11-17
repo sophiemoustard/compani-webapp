4aea1587c9ae33f0fa961184a992cc79b5d49423
KennyCallegari <mel.kenny@gmail.com>
2020-11-17 12:24:59 +0100

    COM-1633 - Fix review

--- src/modules/vendor/pages/ni/management/ELearningCourseProfile.vue
+++ src/modules/vendor/pages/ni/management/ELearningCourseProfile.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="vendor-background" padding>
-    <ni-profile-header title="ICI LE TITRE DE LA COURSE" />
+    <ni-profile-header :title="courseName" />
     <ni-table-list :data="learners" :columns="columns" :loading="tableLoading" :pagination.sync="pagination">
       <template v-slot:body="{ col }">
         <div v-if="col.name === 'name'">{{ col.value }}</div>
@@ -12,9 +12,10 @@
 
 <script>
 import Courses from '@api/Courses';
+import get from 'lodash/get';
 import ProfileHeader from 'src/modules/vendor/components/ProfileHeader';
 import TableList from '@components/table/TableList';
-import { formatIdentity } from '@helpers/utils';
+import { sortStrings, formatIdentity } from '@helpers/utils';
 
 export default {
   name: 'ELearningCoursesProfile',
@@ -32,9 +33,11 @@ export default {
         {
           name: 'name',
           label: 'Nom',
-          field: 'name',
+          field: 'identity',
+          format: value => (value ? value.fullName : ''),
           align: 'left',
           sortable: true,
+          sort: (a, b) => sortStrings(a.lastname, b.lastname),
           style: 'min-width: 200px; width: 70%',
         },
         {
@@ -43,11 +46,13 @@ export default {
           field: 'progress',
           align: 'left',
           sortable: true,
+          sort: (a, b) => b - a,
           style: 'min-width: 110px; width: 10%',
         },
       ],
       learners: [],
-      pagination: { sortBy: 'name', descending: true, page: 1, rowsPerPage: 15 },
+      pagination: { sortBy: 'identity', ascending: true, page: 1, rowsPerPage: 15 },
+      courseName: '',
     };
   },
   async created () {
@@ -55,15 +60,19 @@ export default {
   },
   methods: {
     formatRow (trainee) {
+      const formattedName = formatIdentity(trainee.identity, 'FL');
       const progress = trainee.followUp.reduce((acc, followUp) => followUp.progress + acc, 0) / trainee.followUp.length;
 
-      return ({ name: formatIdentity(trainee.identity, 'FL'), progress });
+      return ({ identity: { ...trainee.identity, fullName: formattedName }, progress });
     },
     async getLearnersList () {
       try {
         this.tableLoading = true;
-        const course = await Courses.getById(this.courseId);
-        if (course) this.learners = Object.freeze(course.trainees.map(this.formatRow));
+        const course = await Courses.getFollowUp(this.courseId);
+        if (course) {
+          this.courseName = get(course, 'subProgram.program.name');
+          this.learners = Object.freeze(course.trainees.map(this.formatRow));
+        }
       } catch (e) {
         console.error(e);
         this.learnerList = [];
