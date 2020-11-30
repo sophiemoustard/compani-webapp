3499189038efe0ebcd0c904d7bcd5d31badd92d0
KennyCallegari <mel.kenny@gmail.com>
2020-11-30 20:36:52 +0100

    COM-1655 - open modal and add access rule on click

--- src/modules/vendor/components/courses/ProfileAccess.vue
+++ src/modules/vendor/components/courses/ProfileAccess.vue
@@ -20,22 +20,34 @@
         </template>
       </ni-responsive-table>
       <q-card-actions align="right">
-        <ni-button color="primary" icon="add" label="Ajouter une règle" :disable="tableLoading" />
+        <ni-button color="primary" icon="add" label="Ajouter une règle" :disable="tableLoading"
+          @click="openAddAccessRuleModal" />
       </q-card-actions>
     </q-card>
+
+    <access-rule-creation-modal v-model="accessRuleCreationModal" :loading="modalLoading" @submit="addAccessRule"
+      :validations="$v.newAccessRule" @hide="resetAccessRuleCreationModal" :new-access-rule="newAccessRule"
+      :company-options="companyOptions" />
   </div>
 </template>
 
 <script>
+import { required } from 'vuelidate/lib/validators';
 import { mapState } from 'vuex';
 import ResponsiveTable from '@components/table/ResponsiveTable';
 import Button from '@components/Button';
+import Companies from '@api/Companies';
+import Courses from '@api/Courses';
+import { formatAndSortOptions } from '@helpers/utils';
+import AccessRuleCreationModal from 'src/modules/vendor/components/courses/AccessRuleCreationModal';
+import { NotifyPositive, NotifyNegative, NotifyWarning } from '@components/popup/notify';
 
 export default {
   name: 'ProfileAccess',
   components: {
     'ni-responsive-table': ResponsiveTable,
     'ni-button': Button,
+    'access-rule-creation-modal': AccessRuleCreationModal,
   },
   props: {
     profileId: { type: String, required: true },
@@ -54,7 +66,65 @@ export default {
         { name: 'actions', label: '', field: '_id' },
       ],
       pagination: { sortBy: 'name', ascending: true, page: 1, rowsPerPage: 50 },
+      newAccessRule: '',
+      companyOptions: [],
+      accessRuleCreationModal: false,
+      modalLoading: false,
     };
+  },
+  validations () {
+    return {
+      newAccessRule: { required },
+    };
+  },
+  methods: {
+    async refreshCourse () {
+      try {
+        await this.$store.dispatch('course/fetchCourse', { courseId: this.profileId });
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    async openAddAccessRuleModal () {
+      try {
+        const companies = await Companies.list();
+        const companiesWithoutAccessRules = companies.filter(
+          c => !this.accessRules.map(ar => ar.name).includes(c.name)
+        );
+
+        this.companyOptions = formatAndSortOptions(companiesWithoutAccessRules, 'name');
+        this.accessRuleCreationModal = true;
+      } catch (e) {
+        console.error(e);
+        this.accessRuleCreationModal = false;
+        this.companyOptions = [];
+      }
+    },
+    resetAccessRuleCreationModal () {
+      this.companyOptions = [];
+      this.newAccessRule = '';
+      this.$v.newAccessRule.$reset();
+    },
+    async addAccessRule (accessRule) {
+      this.newAccessRule = accessRule;
+      try {
+        this.$v.newAccessRule.$touch();
+        if (this.$v.newAccessRule.$error) return NotifyWarning('Une règle d\'accès est requise');
+
+        this.modalLoading = true;
+        await Courses.addAccessRule(this.profileId, { company: accessRule });
+
+        this.accessRuleCreationModal = false;
+        NotifyPositive('Règle d\'accès créé.');
+
+        this.refreshCourse();
+      } catch (e) {
+        console.error(e);
+        NotifyNegative('Erreur lors de la création de la règle d\'accès.');
+      } finally {
+        this.modalLoading = false;
+      }
+    },
   },
 };
 </script>
