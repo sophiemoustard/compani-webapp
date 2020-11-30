2e403fac756dfb4c27a48a2223286569eea3ca9e
Sophie Moustard <sophie.moustard@gmail.com>
2020-11-30 14:41:33 +0100

    remove internal hour put route

--- src/core/api/InternalHours.js
+++ src/core/api/InternalHours.js
@@ -8,9 +8,6 @@ export default {
     const internalHours = await alenviAxios.get(`${process.env.API_HOSTNAME}/internalhours`, { params });
     return internalHours.data.data.internalHours;
   },
-  async update (id, payload) {
-    await alenviAxios.put(`${process.env.API_HOSTNAME}/internalhours/${id}`, payload);
-  },
   async remove (id) {
     await alenviAxios.delete(`${process.env.API_HOSTNAME}/internalhours/${id}`);
   },
