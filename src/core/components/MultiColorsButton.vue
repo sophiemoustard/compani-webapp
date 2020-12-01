c2cc854ce5af5a8710acf6066fd9a7355f9a48e3
KennyCallegari <mel.kenny@gmail.com>
2020-12-01 18:35:16 +0100

    COM-1662 - Add MultiColorsButton component and use it for historyFeed

---
+++ src/core/components/MultiColorsButton.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="button-container">
+    <ni-button class="button" flat :icon="icon" color="primary" @click="click" :size="size" />
+    <ni-button class="button" flat :label="label" color="black" @click="click" :size="size" />
+  </div>
+</template>
+
+<script>
+import Button from '@components/Button';
+
+export default {
+  name: 'MultiColorsButton',
+  props: {
+    icon: { type: String, default: '' },
+    label: { type: String, default: '' },
+    size: { type: String, default: 'md' },
+  },
+  components: {
+    'ni-button': Button,
+  },
+  methods: {
+    click (event) {
+      this.$emit('click', event);
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+  .button-container
+    align-self: flex-end
+
+  .button
+    margin: -4px
+    align-self: center
+
+</style>
