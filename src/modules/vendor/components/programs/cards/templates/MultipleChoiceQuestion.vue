2abfa945e086094d09216fbb55c875f29084b77d
KennyCallegari <mel.kenny@gmail.com>
2020-12-09 15:46:45 +0100

    COM-1691 - refacto qcm

--- src/modules/vendor/components/programs/cards/templates/MultipleChoiceQuestion.vue
+++ src/modules/vendor/components/programs/cards/templates/MultipleChoiceQuestion.vue
@@ -1,15 +1,15 @@
 <template>
-  <div v-if="answersInitialized">
+  <div>
     <ni-input caption="Question" v-model="card.question" required-field @focus="saveTmp('question')"
       @blur="updateCard('question')" :error="$v.card.question.$error" :error-message="questionErrorMsg"
       type="textarea" :disable="disableEdition" />
     <div class="q-my-xl">
-      <div v-for="(qcmAnswers, i) in card.qcmAnswers" :key="i" class="answers">
-        <ni-input :caption="`Réponse ${i + 1}`" v-model="card.qcmAnswers[i].label" :required-field="i < 2"
-          @focus="saveTmp(`qcmAnswers[${i}].label`)" @blur="updateQcmAnswer(i)"
+      <div v-for="(qAnswer, i) in card.qAnswers" :key="i" class="answers">
+        <ni-input :caption="`Réponse ${i + 1}`" v-model="card.qAnswers[i].text" :required-field="i < 2"
+          @focus="saveTmp(`qAnswers[${i}].text`)" @blur="updateAnswer(i)"
           :error="answersError(i)" :error-message="answersErrorMsg(i)" :disable="disableEdition" />
-        <q-checkbox v-model="card.qcmAnswers[i].correct" @input="updateCorrectAnswer(i)"
-          :disable="!card.qcmAnswers[i].label || disableEdition" />
+        <q-checkbox v-model="card.qAnswers[i].correct" @input="updateCorrectAnswer(i)"
+          :disable="!card.qAnswers[i].text || disableEdition" />
       </div>
     </div>
     <ni-input caption="Correction" v-model="card.explanation" required-field @focus="saveTmp('explanation')"
@@ -18,15 +18,13 @@
 </template>
 
 <script>
-import times from 'lodash/times';
 import get from 'lodash/get';
 import { required, maxLength } from 'vuelidate/lib/validators';
 import Cards from '@api/Cards';
 import Input from '@components/form/Input';
 import { NotifyNegative, NotifyPositive, NotifyWarning } from '@components/popup/notify';
-import { MULTIPLE_CHOICE_QUESTION_MAX_ANSWERS_COUNT, REQUIRED_LABEL, QUESTION_MAX_LENGTH,
-  QC_ANSWER_MAX_LENGTH } from '@data/constants';
-import { minLabelArrayLength, minOneCorrectAnswer } from '@helpers/vuelidateCustomVal';
+import { REQUIRED_LABEL, QUESTION_MAX_LENGTH, QC_ANSWER_MAX_LENGTH } from '@data/constants';
+import { minTextArrayLength, minOneCorrectAnswer } from '@helpers/vuelidateCustomVal';
 import { templateMixin } from 'src/modules/vendor/mixins/templateMixin';
 
 export default {
@@ -42,54 +40,37 @@ export default {
     return {
       card: {
         question: { required, maxLength: maxLength(QUESTION_MAX_LENGTH) },
-        qcmAnswers: {
-          minLength: minLabelArrayLength(2),
+        qAnswers: {
+          minLength: minTextArrayLength(2),
           minOneCorrectAnswer,
           $each: {
-            label: { maxLength: maxLength(QC_ANSWER_MAX_LENGTH) },
+            text: { maxLength: maxLength(QC_ANSWER_MAX_LENGTH) },
           },
         },
         explanation: { required },
       },
     };
   },
-  computed: {
-    answersInitialized () {
-      return this.card.qcmAnswers.length === MULTIPLE_CHOICE_QUESTION_MAX_ANSWERS_COUNT;
-    },
-  },
-  watch: {
-    card: {
-      handler: 'initializeAnswers',
-      immediate: true,
-    },
-  },
   methods: {
-    initializeAnswers () {
-      this.card.qcmAnswers = times(
-        MULTIPLE_CHOICE_QUESTION_MAX_ANSWERS_COUNT,
-        i => this.card.qcmAnswers[i] || ({ label: '', correct: false })
-      );
-    },
     requiredAnswerIsMissing (index) {
-      return index < 2 && !this.card.qcmAnswers[index].label && this.$v.card.qcmAnswers.$error;
+      return index < 2 && !this.card.qAnswers[index].text && this.$v.card.qAnswers.$error;
     },
     requiredOneCorrectAnswer (index) {
-      return !this.$v.card.qcmAnswers.minOneCorrectAnswer && !!this.card.qcmAnswers[index].label;
+      return !this.$v.card.qAnswers.minOneCorrectAnswer && !!this.card.qAnswers[index].text;
     },
     removeSingleCorrectAnswer (index) {
-      return this.card.qcmAnswers.filter(a => a.correct).length === 1 && this.card.qcmAnswers[index].correct &&
-        !this.card.qcmAnswers[index].label;
+      return this.card.qAnswers.filter(a => a.correct).length === 1 && this.card.qAnswers[index].correct &&
+        !this.card.qAnswers[index].text;
     },
     formatAnswersPayload () {
-      return { qcmAnswers: this.card.qcmAnswers.filter(a => !!a.label).map(a => ({ ...a, label: a.label.trim() })) };
+      return { qAnswers: this.card.qAnswers.filter(a => !!a.text).map(a => ({ ...a, text: a.text.trim() })) };
     },
-    async updateQcmAnswer (index) {
+    async updateAnswer (index) {
       try {
-        if (this.tmpInput === get(this.card, `qcmAnswers[${index}].label`)) return;
+        if (this.tmpInput === get(this.card, `qAnswers[${index}].text`)) return;
 
-        this.$v.card.qcmAnswers.$touch();
-        if (this.requiredAnswerIsMissing(index) || this.$v.card.qcmAnswers.$each[index].$error) {
+        this.$v.card.qAnswers.$touch();
+        if (this.requiredAnswerIsMissing(index) || this.$v.card.qAnswers.$each[index].$error) {
           return NotifyWarning('Champ(s) invalide(s)');
         }
         if (this.requiredOneCorrectAnswer(index) || this.removeSingleCorrectAnswer(index)) {
@@ -107,7 +88,7 @@ export default {
     },
     async updateCorrectAnswer (index) {
       try {
-        if (!this.card.qcmAnswers[index].label || this.$v.card.qcmAnswers.$each[index].$error) {
+        if (!this.card.qAnswers[index].text || this.$v.card.qAnswers.$each[index].$error) {
           return NotifyWarning('Champ(s) invalide(s)');
         }
         if (this.requiredOneCorrectAnswer(index)) return NotifyWarning('Une bonne réponse est nécessaire.');
@@ -126,13 +107,13 @@ export default {
       if (this.requiredOneCorrectAnswer(index) || this.removeSingleCorrectAnswer(index)) {
         return 'Une bonne réponse est nécessaire.';
       }
-      if (!this.$v.card.qcmAnswers.$each[index].label.maxLength) {
+      if (!this.$v.card.qAnswers.$each[index].text.maxLength) {
         return `${QC_ANSWER_MAX_LENGTH} caractères maximum.`;
       }
       return '';
     },
     answersError (index) {
-      const exceedCharLength = this.$v.card.qcmAnswers.$each[index].$error;
+      const exceedCharLength = this.$v.card.qAnswers.$each[index].$error;
       const missingField = this.requiredAnswerIsMissing(index) || this.requiredOneCorrectAnswer(index) ||
         this.removeSingleCorrectAnswer(index);
       return exceedCharLength || missingField;
