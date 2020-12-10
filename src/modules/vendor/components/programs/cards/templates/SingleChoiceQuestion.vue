5ec85bc6a56077340173b2817bc539085a114609
KennyCallegari <mel.kenny@gmail.com>
2020-12-10 19:12:58 +0100

    COM-1691 - use updateTextAnswer in SingleChoiceQuestion

--- src/modules/vendor/components/programs/cards/templates/SingleChoiceQuestion.vue
+++ src/modules/vendor/components/programs/cards/templates/SingleChoiceQuestion.vue
@@ -8,9 +8,9 @@
       @blur="updateCard('qcuGoodAnswer')" :disable="disableEdition" />
     <div class="q-my-lg">
       <ni-input v-for="(answer, i) in card.qcAnswers" :key="i" :caption="`Mauvaise réponse ${i + 1}`"
-        v-model="card.qcAnswers[i].text" :required-field="i === 0" :error="qcuFalsyAnswerError(i)"
+        v-model="card.qcAnswers[i].text" :required-field="i === 0" :error="$v.card.qcAnswers.$each[i].$error"
         :error-message="qcuFalsyAnswerErrorMsg(i)" @focus="saveTmp(`qcAnswers[${i}].text`)"
-        @blur="updateFalsyAnswer(i)" :disable="disableEdition" />
+        @blur="updateTextAnswer(i)" :disable="disableEdition" />
     </div>
     <ni-input caption="Correction" v-model="card.explanation" required-field @focus="saveTmp('explanation')"
       @blur="updateCard('explanation')" :error="$v.card.explanation.$error" type="textarea" :disable="disableEdition" />
@@ -18,12 +18,9 @@
 </template>
 
 <script>
-import get from 'lodash/get';
 import { required, maxLength } from 'vuelidate/lib/validators';
 import Input from '@components/form/Input';
-import { NotifyNegative, NotifyPositive, NotifyWarning } from '@components/popup/notify';
 import { REQUIRED_LABEL, QUESTION_MAX_LENGTH, QC_ANSWER_MAX_LENGTH } from '@data/constants';
-import { minTextArrayLength, minOneCorrectAnswer } from '@helpers/vuelidateCustomVal';
 import { validationMixin } from '@mixins/validationMixin';
 import { templateMixin } from 'src/modules/vendor/mixins/templateMixin';
 
@@ -42,10 +39,8 @@ export default {
         question: { required, maxLength: maxLength(QUESTION_MAX_LENGTH) },
         qcuGoodAnswer: { required, maxLength: maxLength(QC_ANSWER_MAX_LENGTH) },
         qcAnswers: {
-          minLength: minTextArrayLength(1),
-          minOneCorrectAnswer,
           $each: {
-            text: { maxLength: maxLength(QC_ANSWER_MAX_LENGTH) },
+            text: { required, maxLength: maxLength(QC_ANSWER_MAX_LENGTH) },
           },
         },
         explanation: { required },
@@ -55,39 +50,17 @@ export default {
   computed: {
     goodAnswerErrorMsg () {
       if (!this.$v.card.qcuGoodAnswer.required) return REQUIRED_LABEL;
-      if (!this.$v.card.qcuGoodAnswer.maxLength) {
-        return `${QC_ANSWER_MAX_LENGTH} caractères maximum.`;
-      }
+      if (!this.$v.card.qcuGoodAnswer.maxLength) return `${QC_ANSWER_MAX_LENGTH} caractères maximum.`;
+
       return '';
     },
   },
   methods: {
     qcuFalsyAnswerErrorMsg (index) {
-      if (!this.$v.card.qcAnswers.$each[index].text.maxLength) {
-        return `${QC_ANSWER_MAX_LENGTH} caractères maximum.`;
-      }
+      if (!this.$v.card.qcAnswers.$each[index].text.required) return REQUIRED_LABEL;
+      if (!this.$v.card.qcAnswers.$each[index].text.maxLength) return `${QC_ANSWER_MAX_LENGTH} caractères maximum.`;
+
       return '';
-    },
-    qcuFalsyAnswerError (index) {
-      const exceedCharLength = this.$v.card.qcAnswers.$each[index].text.$error;
-      const missingField = this.$v.card.qcAnswers.$error &&
-        !this.$v.card.qcAnswers.minLength && index === 0 && !this.card.qcAnswers[index].text;
-
-      return exceedCharLength || missingField;
-    },
-    async updateFalsyAnswer (index) {
-      try {
-        if (this.tmpInput === get(this.card, `qcAnswers[${index}].text`)) return;
-
-        this.$v.card.qcAnswers.$touch();
-        if (this.qcuFalsyAnswerError(index)) return NotifyWarning('Champ(s) invalide(s)');
-
-        await this.refreshCard();
-        NotifyPositive('Carte mise à jour.');
-      } catch (e) {
-        console.error(e);
-        NotifyNegative('Erreur lors de la mise à jour de la carte.');
-      }
     },
   },
 };
