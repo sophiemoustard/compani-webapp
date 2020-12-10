41a5c3f36081a7ad9f6eaa8abb8571be081c9812
Félix <felix@alenvi.io>
2020-12-10 15:21:10 +0100

    COM-1694 add percentage

--- src/core/components/CourseProgress.vue
+++ src/core/components/CourseProgress.vue
@@ -1,18 +1,32 @@
 <template>
-  <q-linear-progress class="linear-progress" size="8px" :value="value" color="primary" />
+  <div class="container">
+    <q-linear-progress class="linear-progress" size="8px" :value="value" color="primary" />
+    <a class="q-mx-md percentage">{{ percentage }}</a>
+  </div>
 </template>
 
 <script>
+import { roundFrenchPercentage } from '@helpers/utils';
+
 export default {
   name: 'Progress',
   props: {
     value: { type: Number, default: 0 },
   },
+  computed: {
+    percentage () {
+      return roundFrenchPercentage(this.value * 100, 0);
+    },
+  },
 };
 </script>
 
 <style lang="stylus" scoped>
+.container
+  display: ruby
+
 .linear-progress
+  width: 104px
   border-radius: 100px
   background-color: $primary-light
 </style>
