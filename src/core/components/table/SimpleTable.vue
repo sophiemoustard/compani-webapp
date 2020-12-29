a9b78df6ab4f54563f3751ab13e1195a0b49ab90
Félix <felix@alenvi.io>
2020-12-29 14:18:34 +0100

    COM-1737 replace class grey-text & grey-background

--- src/core/components/table/SimpleTable.vue
+++ src/core/components/table/SimpleTable.vue
@@ -20,7 +20,7 @@
         <slot name="bottom-row" :props="props" />
       </template>
       <template #no-data>
-        <div v-show="!loading" class="full-width row q-gutter-sm grey-text">
+        <div v-show="!loading" class="full-width row q-gutter-sm text-grey-800">
           <span>Pas de données disponibles</span>
         </div>
       </template>
