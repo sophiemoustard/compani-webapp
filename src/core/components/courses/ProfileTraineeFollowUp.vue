df86c95fb8e1e81a972232ca6923511a0b63f97e
Félix <felix@alenvi.io>
2021-01-05 16:14:42 +0100

    COM-1751 simple table with static button

--- src/core/components/courses/ProfileTraineeFollowUp.vue
+++ src/core/components/courses/ProfileTraineeFollowUp.vue
@@ -1,31 +1,63 @@
 <template>
-  <div v-if="followUp.subProgram">
-    <div v-for="(step, stepIndex) of followUp.subProgram.steps" :key="stepIndex" class="q-mb-xl">
-      <div class="text-weight-bold q-mb-sm">{{ stepIndex + 1 }} - {{ step.name }}</div>
-      <q-card v-for="(activity, activityIndex) of step.activities" :key="activityIndex" flat class="q-mb-sm">
-        <q-card-section class="cursor-pointer" @click="showCards(activity._id)">
-          <div class="text-weight-bold">{{ activityIndex + 1 }} - {{ activity.name }}</div>
-        </q-card-section>
-        <div class="bg-peach-200 chart-container" v-if="areCardsDisplayed[activity._id]">
-          <template v-for="(card, cardIndex) of activity.followUp">
-            <survey-chart v-if="card.template === SURVEY" :key="cardIndex" class="chart" :card="card" />
-            <open-question-chart v-if="card.template === OPEN_QUESTION" :key="cardIndex" class="chart" :card="card" />
-            <question-answer-chart v-if="card.template === QUESTION_ANSWER" :key="cardIndex" class="chart"
-              :card="card" />
-          </template>
-        </div>
-      </q-card>
+  <div>
+    <div class="q-my-md">
+      <p class="text-weight-bold">Émargements</p>
+      <ni-simple-table :data="attendanceSheets" :columns="columns">
+        <template #body="{ props }">
+          <q-tr :props="props">
+            <q-td :props="props" v-for="col in props.cols" :key="col.name" :data-label="col.label" :class="col.name"
+              :style="col.style">
+              <template v-if="col.name === 'actions'">
+                <div class="row no-wrap table-actions justify-end">
+                  <ni-button icon="download" color="primary" disabled />
+                  <ni-button icon="delete" color="primary" disabled />
+                </div>
+              </template>
+              <template v-else>
+                {{ col.value }}
+              </template>
+            </q-td>
+          </q-tr>
+        </template>
+      </ni-simple-table>
+      <div class="flex justify-end">
+        <ni-button class="bg-primary" color="white" icon="add" label="Ajouter une feuille d'émargement"
+          @click="addAttendanceSheet()" />
+      </div>
+    </div>
+    <div v-if="followUp.subProgram">
+      <p class="text-weight-bold">Progression des participants</p>
+      <div v-for="(step, stepIndex) of followUp.subProgram.steps" :key="stepIndex" class="q-mb-xl">
+        <div class="q-mb-sm">{{ stepIndex + 1 }} - {{ step.name }}</div>
+        <q-card v-for="(activity, activityIndex) of step.activities" :key="activityIndex" flat class="q-mb-sm">
+          <q-card-section class="cursor-pointer" @click="showCards(activity._id)">
+            <div class="text-weight-bold">{{ activityIndex + 1 }} - {{ activity.name }}</div>
+          </q-card-section>
+          <div class="bg-peach-200 chart-container" v-if="areCardsDisplayed[activity._id]">
+            <template v-for="(card, cardIndex) of activity.followUp">
+              <survey-chart v-if="card.template === SURVEY" :key="cardIndex" class="chart" :card="card" />
+              <open-question-chart v-if="card.template === OPEN_QUESTION" :key="cardIndex" class="chart" :card="card" />
+              <question-answer-chart v-if="card.template === QUESTION_ANSWER" :key="cardIndex" class="chart"
+                :card="card" />
+            </template>
+          </div>
+        </q-card>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import moment from '@helpers/moment';
 import Courses from '@api/Courses';
-import { NotifyNegative } from '@components/popup/notify';
+import { NotifyPositive, NotifyNegative } from '@components/popup/notify';
 import SurveyChart from '@components/courses/SurveyChart';
 import OpenQuestionChart from '@components/courses/OpenQuestionChart';
 import { SURVEY, OPEN_QUESTION, QUESTION_ANSWER } from '@data/constants';
 import QuestionAnswerChart from '@components/courses/QuestionAnswerChart';
+import SimpleTable from '@components/table/SimpleTable';
+import AttendanceSheets from '@api/AttendanceSheets';
+import Button from '@components/Button';
 
 export default {
   name: 'ProfileTraineeFollowUp',
@@ -33,6 +65,8 @@ export default {
     'survey-chart': SurveyChart,
     'open-question-chart': OpenQuestionChart,
     'question-answer-chart': QuestionAnswerChart,
+    'ni-simple-table': SimpleTable,
+    'ni-button': Button,
   },
   props: {
     profileId: { type: String, required: true },
@@ -41,6 +75,24 @@ export default {
     return {
       followUp: {},
       areCardsDisplayed: {},
+      attendanceSheetsLoading: false,
+      attendanceSheets: [],
+      columns: [
+        {
+          name: 'date',
+          label: 'Date',
+          align: 'left',
+          field: 'date',
+          format: value => (value ? moment(value).format('DD/MM/YYYY') : ''),
+        },
+        {
+          name: 'trainee',
+          label: 'Nom de l\'apprenant',
+          align: 'left',
+          field: 'trainee',
+        },
+        { name: 'actions', label: '', align: 'left', field: row => row },
+      ],
       SURVEY,
       OPEN_QUESTION,
       QUESTION_ANSWER,
@@ -48,6 +100,7 @@ export default {
   },
   async created () {
     await this.refreshFollowUp();
+    await this.refreshAttendanceSheets();
   },
   methods: {
     async refreshFollowUp () {
@@ -59,8 +112,38 @@ export default {
         console.error(e);
       }
     },
+    async refreshAttendanceSheets () {
+      try {
+        this.attendanceSheetsLoading = true;
+        this.attendanceSheets = await AttendanceSheets.list({ course: this.profileId });
+      } catch (e) {
+        console.error(e);
+        this.attendanceSheets = [];
+        NotifyNegative('Erreur lors de la récupération des feuilles d\'émargement.');
+      } finally {
+        this.attendanceSheetsLoading = false;
+      }
+    },
     showCards (activityId) {
       this.$set(this.areCardsDisplayed, activityId, !this.areCardsDisplayed[activityId]);
+    },
+    async addAttendanceSheet () {
+      try {
+        await AttendanceSheets.create({
+          course: this.profileId,
+          link: 'https://google.com',
+          trainee: this.profileId,
+          date: moment(),
+        });
+
+        NotifyPositive('Feuille d\'émargement ajoutée.');
+        await this.refreshAttendanceSheets();
+      } catch (e) {
+        console.error(e);
+        NotifyNegative('Erreur lors de l\'ajout de la feuille d\'émargement.');
+      } finally {
+        this.modalLoading = false;
+      }
     },
   },
 };
