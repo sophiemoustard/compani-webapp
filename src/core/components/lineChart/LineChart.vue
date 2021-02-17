501427e88dbbb1c33950cc717444d478a7eb3005
KennyCallegari <mel.kenny@gmail.com>
2021-02-17 18:22:40 +0100

    COM-1844 - Add new component LineChart

---
+++ src/core/components/lineChart/LineChart.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <q-card flat class="q-pa-md">
+      <div class="text-weight-bold q-mb-md">{{ title }}</div>
+      <line-chart :chart-data="chartData" :options="options" class="line-chart" />
+    </q-card>
+  </div>
+</template>
+
+<script>
+import LineChart from '@components/lineChart/VueChartLineChart';
+
+export default {
+  name: 'ProfileFollowUp',
+  components: {
+    'line-chart': LineChart,
+  },
+  props: {
+    title: { type: String, required: true },
+    chartData: { type: Object, required: true },
+    options: { type: Object, required: true },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+.line-chart
+  max-height: 200px;
+</style>
