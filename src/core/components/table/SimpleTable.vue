ec1618d64634a4f83554841087a70930248700f7
ulysse <ulysse@alenvi.io>
2021-02-26 11:03:42 +0100

    COM-1886: fix sticky header bug on tobill

--- src/core/components/table/SimpleTable.vue
+++ src/core/components/table/SimpleTable.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="relative-position table-spinner-container">
     <q-table v-if="!loading" :data="data" :columns="columns" :pagination="pagination" binary-state-sort
-      class="q-pa-sm large-table sticky-header-table" flat :separator="separator" :selection="selection"
-      :row-key="rowKey" v-on="$listeners" :selected="selected" :visible-columns="formattedVisibleColumns"
-      :hide-bottom="shouldHideBottom">
+      flat :separator="separator" :selection="selection" :row-key="rowKey" v-on="$listeners" :selected="selected"
+      :visible-columns="formattedVisibleColumns" :hide-bottom="shouldHideBottom"
+      :class="['q-pa-sm large-table', isClientInterface ?
+        'client-sticky-header'
+        : 'vendor-sticky-header']">
       <template #header="props">
         <slot name="header" :props="props">
           <q-tr :props="props">
@@ -65,6 +67,12 @@ export default {
   components: {
     'ni-pagination': Pagination,
   },
+  data () {
+    const isClientInterface = !/\/ad\//.test(this.$router.currentRoute.path);
+    return {
+      isClientInterface,
+    };
+  },
   computed: {
     shouldHideBottom () {
       return this.hideBottom || (!!this.data.length && this.data.length <= this.rowsPerPage[0]);
@@ -83,20 +91,3 @@ export default {
   },
 };
 </script>
-
-<style lang="stylus" scoped>
-  .sticky-header-table
-    @media screen and (max-height : 735px)
-      height: 70vh
-    @media screen and (min-height : 736px) and (max-height : 1023px)
-      height: 85vh
-    @media screen and (min-height : 1024px)
-      height: 65vh
-
-    thead tr th
-      position: sticky
-      z-index: 1
-      background-color: $grey-100
-    thead tr:first-child th
-      top: 0
-</style>
