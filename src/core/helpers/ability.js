bac8d61ef258f21cf6e60c37019a653c7547b1a6
Sophie Moustard <sophie.moustard@gmail.com>
2021-03-11 12:14:57 +0100

    COM-1950 - use set keyword as cannot update

--- src/core/helpers/ability.js
+++ src/core/helpers/ability.js
@@ -30,7 +30,7 @@ export const defineAbilitiesFor = (user) => {
   if (clientRole) can('read', getClientAbilities(clientRole, companySubscriptions));
   if (vendorRole) can('read', getVendorAbilities(vendorRole));
   if (!clientRole && !vendorRole) can('read', 'account client');
-  if ([VENDOR_ADMIN, TRAINING_ORGANISATION_MANAGER].includes(vendorRole)) can('update', 'user_company');
+  if ([VENDOR_ADMIN, TRAINING_ORGANISATION_MANAGER].includes(vendorRole)) can('set', 'user_company');
 
   if ([VENDOR_ADMIN, TRAINING_ORGANISATION_MANAGER].includes(vendorRole) ||
       [CLIENT_ADMIN, COACH, PLANNING_REFERENT].includes(clientRole)) can('read', 'learner_info');
