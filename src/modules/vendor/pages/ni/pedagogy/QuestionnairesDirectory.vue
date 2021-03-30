ade4ea7b3ae693e41d225cca9974f31f5284a448
ulysse <ulysse@alenvi.io>
2021-03-30 12:15:36 +0200

    COM-1973: add questionnaire section to menu

---
+++ src/modules/vendor/pages/ni/pedagogy/QuestionnairesDirectory.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="vendor-background" padding>
+    <ni-title-header title="Questionnaires" class="row q-mb-xl" />
+  </q-page>
+</template>
+
+<script>
+import TitleHeader from '@components/TitleHeader';
+
+export default {
+  metaInfo: { title: 'Catégories' },
+  name: 'QuestionnairesDirectory',
+  components: {
+    'ni-title-header': TitleHeader,
+  },
+};
+</script>
