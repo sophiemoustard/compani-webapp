e93a38e0c98b153570c4f00d4d93c8d20511cdb0
ulysse <ulysse@alenvi.io>
2021-04-01 16:41:36 +0200

    COM-1974: remove useless constant

--- src/core/data/constants.js
+++ src/core/data/constants.js
@@ -386,12 +386,6 @@ export const CARD_TEMPLATES = [
   { label: 'Question\t&\tRéponse', value: QUESTION_ANSWER, type: QUESTIONNAIRE },
 ];
 
-export const TEMPLATE_TYPES = {
-  [LESSON]: 'Cours',
-  [QUIZ]: 'Quiz',
-  [QUESTIONNAIRE]: 'Questionnaire',
-};
-
 export const SINGLE_CHOICE_QUESTION_MAX_FALSY_ANSWERS_COUNT = 3;
 export const SINGLE_CHOICE_QUESTION_MIN_FALSY_ANSWERS_COUNT = 1;
 export const MULTIPLE_CHOICE_QUESTION_MAX_ANSWERS_COUNT = 4;
