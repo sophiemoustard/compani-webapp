8cdef46c445e96b9c590805ca129f21049d3454f
ulysse <ulysse@alenvi.io>
2021-04-02 11:37:10 +0200

    COM-1975: display header

--- src/modules/vendor/pages/ni/pedagogy/QuestionnaireProfile.vue
+++ src/modules/vendor/pages/ni/pedagogy/QuestionnaireProfile.vue
@@ -1,17 +1,45 @@
 <template>
-  <q-page padding class="vendor-background" />
+  <q-page padding class="vendor-background">
+    <template v-if="questionnaire">
+      <ni-profile-header :title="questionnaire.title">
+        <template #body>
+          <div class="row profile-info q-pl-lg">
+            <q-item v-for="info of headerInfo" class="col-md-6 col-xs-12" :key="info.icon">
+              <q-item-section side>
+                <q-icon size="xs" :name="info.icon" :class="info.class" />
+              </q-item-section>
+              <q-item-section :class="info.class">{{ info.label }}</q-item-section>
+            </q-item>
+          </div>
+        </template>
+      </ni-profile-header>
+    </template>
+  </q-page>
 </template>
 
 <script>
 import { mapState } from 'vuex';
+import ProfileHeader from '@components/ProfileHeader';
+import { DRAFT, QUESTIONNAIRE_TYPES } from '@data/constants';
 
 export default {
   name: 'QuestionnaireProfile',
   props: {
     questionnaireId: { type: String, required: true },
   },
+  components: {
+    'ni-profile-header': ProfileHeader,
+  },
   computed: {
     ...mapState('questionnaire', ['questionnaire']),
+    headerInfo () {
+      const infos = [{ icon: 'bookmark_border', label: QUESTIONNAIRE_TYPES[this.questionnaire.type] }];
+
+      if (this.questionnaire.status === DRAFT) infos.push({ icon: 'edit', label: 'Brouillon', class: 'info-warning' });
+      else infos.push({ icon: 'check_circle', label: 'Publiée', class: 'info-active' });
+
+      return infos;
+    },
   },
   async created () {
     await this.refreshQuestionnaire();
@@ -23,3 +51,11 @@ export default {
   },
 };
 </script>
+
+<style lang="stylus" scoped>
+.info
+  &-active
+    color: $green-800
+  &-warning
+    color: $orange-500
+</style>
