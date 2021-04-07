3f214e8cb74006227436cf2c99e4c26bf970bb1e
KennyCallegari <mel.kenny@gmail.com>
2021-04-07 20:33:41 +0200

    COM-1977 - Add table

--- src/modules/client/pages/ni/customers/PartnerOrganizationsDirectory.vue
+++ src/modules/client/pages/ni/customers/PartnerOrganizationsDirectory.vue
@@ -1,11 +1,17 @@
 <template>
   <q-page class="client-background" padding>
     <ni-title-header title="Structures partenaires" class="q-mb-xl" />
+    <ni-table-list :data="partnerOrganizations" :columns="columns" :pagination.sync="pagination" :loading="tableLoading"
+      :rows-per-page="[15, 50, 100, 200]">
+      <template #body="{ col }">
+        <template>{{ col.value }}</template>
+      </template>
+    </ni-table-list>
     <q-btn class="fixed fab-custom" no-caps rounded color="primary" icon="add" label="Ajouter une structure"
       @click="partnerOrganizationCreationModal = true" />
 
     <partner-organization-creation-modal v-model="partnerOrganizationCreationModal" @submit="createPartnerOrganization"
-      :new-partner-organization.sync="newPartnerOrganization" :loading="loading" @hide="resetModal"
+      :new-partner-organization.sync="newPartnerOrganization" :loading="modalLoading" @hide="resetModal"
       :validations="$v.newPartnerOrganization" />
   </q-page>
 </template>
@@ -15,9 +21,11 @@ import omit from 'lodash/omit';
 import pickBy from 'lodash/pickBy';
 import get from 'lodash/get';
 import TitleHeader from '@components/TitleHeader';
+import TableList from '@components/table/TableList';
 import PartnerOrganization from '@api/PartnerOrganizations';
 import { NotifyPositive, NotifyWarning, NotifyNegative } from '@components/popup/notify';
 import { frPhoneNumber, frAddress } from '@helpers/vuelidateCustomVal';
+import { sortStrings } from '@helpers/utils';
 import PartnerOrganizationCreationModal from 'src/modules/client/components/customers/PartnerOrganizationCreationModal';
 
 export default {
@@ -26,12 +34,26 @@ export default {
   components: {
     'ni-title-header': TitleHeader,
     'partner-organization-creation-modal': PartnerOrganizationCreationModal,
+    'ni-table-list': TableList,
   },
   data () {
     return {
       partnerOrganizationCreationModal: false,
       newPartnerOrganization: { name: '', phone: '', address: {}, email: '' },
-      loading: false,
+      modalLoading: false,
+      tableLoading: false,
+      partnerOrganizations: [],
+      columns: [
+        {
+          name: 'name',
+          label: 'Nom',
+          field: 'name',
+          align: 'left',
+          sortable: true,
+          sort: (a, b) => sortStrings(a, b),
+        },
+      ],
+      pagination: { sortBy: 'createdAt', descending: true, page: 1, rowsPerPage: 15 },
     };
   },
   validations: {
