59a68419368fb016598207e46f6481510d0cf9de
KennyCallegari <mel.kenny@gmail.com>
2021-04-09 15:24:16 +0200

    COM-1978 - Fix design

--- src/modules/vendor/pages/trainers/management/BlendedCoursesDirectory.vue
+++ src/modules/vendor/pages/trainers/management/BlendedCoursesDirectory.vue
@@ -1,21 +1,12 @@
 <template>
   <q-page class="vendor-background" padding>
     <ni-title-header title="Formations" class="q-mb-xl" />
-    <div class="row">
-      <div class="col-xs-12 col-sm-6 col-md-3">
-        <ni-select :options="companyFilterOptions" :value="selectedCompany" @input="updateSelectedCompany" />
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-3">
-        <ni-select :class="{ 'q-pl-sm': $q.platform.is.desktop }" :options="programFilterOptions"
-          :value="selectedProgram" @input="updateSelectedProgram" />
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-3">
-        <ni-select :class="{ 'q-pl-sm': $q.platform.is.desktop }" :options="salesRepresentativesFilterOptions"
-          :value="selectedSalesRepresentative" @input="updateSelectedSalesRepresentative" />
-      </div>
-      <div class="col-xs-12 col-sm-6 col-md-3 reset-filters" @click="resetFilters">
-        <span>Effacer les filtres</span>
-      </div>
+    <div class="filters-container">
+      <ni-select :options="companyFilterOptions" :value="selectedCompany" @input="updateSelectedCompany" />
+      <ni-select :options="programFilterOptions" :value="selectedProgram" @input="updateSelectedProgram" />
+      <ni-select :options="salesRepresentativesFilterOptions"
+        :value="selectedSalesRepresentative" @input="updateSelectedSalesRepresentative" />
+      <div class="reset-filters" @click="resetFilters">Effacer les filtres</div>
     </div>
     <ni-trello :courses="coursesFiltered" />
   </q-page>
