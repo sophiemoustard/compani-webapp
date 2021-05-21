c67609a42c7945b6d49022baea284099196a3cf5
Sophie Moustard <sophie.moustard@gmail.com>
2021-05-21 08:58:38 +0200

    COM-2075 - refacto events

--- src/modules/client/components/planning/Planning.vue
+++ src/modules/client/components/planning/Planning.vue
@@ -138,7 +138,7 @@ export default {
   },
   props: {
     workingStats: { type: Object, default: () => ({}) },
-    events: { type: Array, default: () => [] },
+    events: { type: Object, default: () => ({}) },
     persons: { type: Array, default: () => [] },
     filteredSectors: { type: Array, default: () => [] },
     personKey: { type: String, default: 'auxiliary' },
@@ -249,9 +249,7 @@ export default {
       return total / 60;
     },
     getRowEvents (rowId) {
-      const rowEvents = this.events.find(group => group._id === rowId);
-
-      return (!rowEvents || !rowEvents.events) ? [] : rowEvents.events;
+      return this.events[rowId] || [];
     },
     getCellEvents (cellId, day) {
       return this.getRowEvents(cellId)
