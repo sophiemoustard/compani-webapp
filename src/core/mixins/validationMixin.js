6d73c6919c26a09556df3a8989179dec862fd889
Sophie Moustard <sophie.moustard@gmail.com>
2021-07-29 14:20:24 +0200

    COM-2226 - refacto validation mixins

--- src/core/mixins/validationMixin.js
+++ src/core/mixins/validationMixin.js
@@ -2,32 +2,23 @@ import get from 'lodash/get';
 
 export const validationMixin = {
   methods: {
-    waitForValidation (validationObj, path) {
-      return new Promise((resolve) => {
-        if (path.match(/address/i)) {
-          const unwatch = this.$watch(
-            () => !get(validationObj, path).$pending,
-            (notPending) => {
-              if (notPending) {
-                if (unwatch) unwatch();
-                resolve(!get(validationObj, path).$error);
-              }
-            },
-            { immediate: true }
-          );
-        } else {
-          resolve(!get(validationObj, path).$error);
-        }
-      });
+    async waitForValidation (validationObj, path) {
+      if (path.match(/address/i)) return this.waitForFormValidation(get(validationObj, path));
+
+      return !get(validationObj, path).$error;
     },
-    waitForFormValidation (validationObj) {
+    async waitForFormValidation (validationObj) {
       return new Promise((resolve) => {
-        const unwatch = this.$watch(() => !validationObj.$pending, (notPending) => {
-          if (notPending) {
-            if (unwatch) unwatch();
-            resolve(!validationObj.$error);
-          }
-        }, { immediate: true });
+        const unwatch = this.$watch(
+          () => !validationObj.$pending,
+          (notPending) => {
+            if (notPending) {
+              if (unwatch) unwatch();
+              resolve(!validationObj.$error);
+            }
+          },
+          { immediate: true }
+        );
       });
     },
   },
