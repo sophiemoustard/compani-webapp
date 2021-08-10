98cc3129666fbd095a75bcb83be44784475c2885
ulysse <ulysse@alenvi.io>
2021-08-10 16:55:58 +0200

    COM-2275: add tags

--- src/modules/client/pages/ni/config/TagConfig.vue
+++ src/modules/client/pages/ni/config/TagConfig.vue
@@ -1,14 +1,17 @@
 <template>
   <q-page class="client-background" padding>
     <div class="q-mb-xl">
-      <table>
-        <th>Nom tag</th>
-        <th>Description</th>
-        <tr v-for="(tag, index) in tags" :key="index">
-          <td>{{ `{${index}\}` }}</td>
-          <td>{{ tag }}</td>
-        </tr>
-      </table>
+      <div v-for="(section, i) in tagSections" :key="i" class="q-mb-lg">
+        <div class="text-weight-bold">{{ section.name }}</div>
+        <table>
+          <th>Nom tag</th>
+          <th>Description</th>
+          <tr v-for="(tag, index) in section.items" :key="index">
+            <td>{{ `{${index}\}` }}</td>
+            <td>{{ tag }}</td>
+          </tr>
+        </table>
+      </div>
     </div>
   </q-page>
 </template>
@@ -19,28 +22,7 @@ export default {
   metaInfo: { title: 'Tags' },
   data () {
     return {
-      tags: {
-        customerFirstname: 'Prénom du/de la bénéficiaire',
-        customerLastname: 'Nom du/de la bénéficiaire',
-        customerAddress: 'Adresse du/de la bénéficiaire',
-        ics: 'ICS',
-        rum: 'RUM',
-        iban: 'IBAN',
-        bic: 'BIC',
-        bankAccountOwner: 'Nom associé au compte bancaire',
-        companyName: 'Nom de l’organisation',
-        companyAddress: 'Adresse de l\'organisation',
-        downloadDate: 'Date du téléchargement du contrat pour la date de signature',
-        quoteNumber: 'Numéro du devis',
-        rcs: 'RCS de l’organisation',
-        subscriptions: 'Liste des services souscrits',
-        serviceName: 'Nom du service',
-        serviceNature: 'Nature du service',
-        unitTTCRate: 'Tarif horaire TTC',
-        weeklyVolume: 'Volume hebdomadaire estimé total',
-        sundays: 'Dont dimanche',
-        evenings: 'Dont soir après 20h',
-        weeklyRate: 'Tarif hebdomadaire estimé',
+      contractTags: {
         auxiliaryTitle: 'Civilité',
         auxiliaryFirstname: 'Prénom',
         auxiliaryLastname: 'Nom',
@@ -48,19 +30,75 @@ export default {
         auxiliaryCity: 'Ville',
         auxiliaryZipCode: 'Code postal',
         auxiliaryBirthDate: 'Date de naissance',
+        auxiliaryNationality: 'Nationalité',
         auxiliaryBirthCountry: 'Pays de naissance',
         auxiliaryBirthState: 'Département de naissance',
-        auxiliaryNationality: 'Nationalité',
         auxiliarySSN: 'Numéro de sécurité sociale',
         grossHourlyRate: 'Taux horaire brut',
-        monthlyHours: 'Volume d\'heure mensuel (Volume d\'heures hebdomadaire * 4,33)',
+        monthlyHours: 'Volume d\'heures mensuel (Volume d\'heures hebdomadaire * 4,33)',
         salary: 'Salaire mensuel (Volume d\'heures mensuel * taux horaire brut)',
         startDate: 'Date d\'effet du contrat / de la version',
         weeklyHours: 'Volume horaire hebdomadaire (contrat de travail)',
         yearlyHours: 'Volume horaire annuel (Volume d\'heure mensuel * 52)',
+        uploadDate: 'Date de mise en ligne du contrat',
         initialContractStartDate: 'Date d’effet du contrat initial',
+        initialContractMonthlyHours: 'Volume d\'heures mensuel du contrat initial',
+      },
+      mandateTags: {
+        customerFirstname: 'Prénom du/de la bénéficiaire',
+        customerLastname: 'Nom du/de la bénéficiaire',
+        customerAddress: 'Adresse du/de la bénéficiaire',
+        customerStreet: 'Rue du/de la bénéficiaire',
+        customerCity: 'Ville du/de la bénéficiaire',
+        customerZipCode: 'Code postal du/de la bénéficiaire',
+        ics: 'ICS',
+        companyName: 'Nom de l’organisation',
+        companyAddress: 'Adresse de l\'organisation',
+        companyStreet: 'Rue de l\'organisation',
+        companyCity: 'Ville de l\'organisation',
+        companyZipCode: 'Code postal de l\'organisation',
+        downloadDate: 'Date de téléchargement',
+        rcs: 'RCS de l’organisation',
+        rum: 'RUM',
+        iban: 'IBAN',
+        bic: 'BIC',
+        bankAccountOwner: 'Nom associé au compte bancaire',
+      },
+      quoteTags: {
+        customerFirstname: 'Prénom du/de la bénéficiaire',
+        customerLastname: 'Nom du/de la bénéficiaire',
+        customerAddress: 'Adresse du/de la bénéficiaire',
+        customerStreet: 'Rue du/de la bénéficiaire',
+        customerCity: 'Ville du/de la bénéficiaire',
+        customerZipCode: 'Code postal du/de la bénéficiaire',
+        ics: 'ICS',
+        companyName: 'Nom de l’organisation',
+        companyAddress: 'Adresse de l\'organisation',
+        companyStreet: 'Rue de l\'organisation',
+        companyCity: 'Ville de l\'organisation',
+        companyZipCode: 'Code postal de l\'organisation',
+        downloadDate: 'Date de téléchargement',
+        rcs: 'RCS de l’organisation',
+        serviceName: 'Nom du service',
+        serviceNature: 'Nature du service',
+        unitTTCRate: 'Tarif horaire TTC',
+        weeklyVolume: 'Volume hebdomadaire estimé total',
+        sundays: 'Dont dimanche',
+        evenings: 'Dont soir après 20h',
+        quoteNumber: 'Numéro du devis',
+        subscriptions: 'Liste des services souscrits',
+        weeklyRate: 'Tarif hebdomadaire estimé',
       },
     };
+  },
+  computed: {
+    tagSections () {
+      return [
+        { name: 'Contrat', items: this.contractTags },
+        { name: 'Mandat', items: this.mandateTags },
+        { name: 'Devis', items: this.quoteTags },
+      ];
+    },
   },
 };
 </script>
