2a970352a483832aac845b1fdd60cc8c35671c98
KennyCallegari <mel.kenny@gmail.com>
2021-09-02 16:20:30 +0200

    COM-2298 - Add manual bills route

---
+++ src/modules/client/pages/ni/billing/ManualBills.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="client-background q-pb-xl">
+    <ni-title-header title="Factures manuelles" padding />
+
+    <q-btn class="fixed fab-custom" no-caps rounded color="primary" icon="add" label="Créer une facture" />
+  </q-page>
+</template>
+
+<script>
+import TitleHeader from '@components/TitleHeader';
+
+export default {
+  name: 'ManualBills',
+  metaInfo: { title: 'Factures manuelles' },
+  components: {
+    'ni-title-header': TitleHeader,
+  },
+};
+</script>
