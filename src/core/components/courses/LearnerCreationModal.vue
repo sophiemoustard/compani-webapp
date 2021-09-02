75ce4a29938f34045f804e945ba3030f588e39a7
CorentinMartinot <corentin.martinot@gmail.com>
2021-09-02 14:47:57 +0200

    COM-2301 change name companiStep

--- src/core/components/courses/LearnerCreationModal.vue
+++ src/core/components/courses/LearnerCreationModal.vue
@@ -14,11 +14,11 @@
           :error="validations.identity.lastname.$error" />
         <ni-input in-modal :value="newUser.contact.phone" @input="update($event.trim(), 'contact.phone')"
           caption="Téléphone" @blur="validations.contact.phone.$touch" :error="validations.contact.phone.$error"
-          :error-message="phoneNbrError(validations)" :last="!companyStep" required-field />
-      </template>
-        <ni-select v-if="!firstStep && companyStep" in-modal :options="companyOptions" :value="newUser.company"
+          :error-message="phoneNbrError(validations)" :last="!displayCompany" required-field />
+        <ni-select v-if="displayCompany" in-modal :options="companyOptions" :value="newUser.company"
           @input="update($event.trim(), 'company')" required-field caption="Structure"
-          @blur="validations.company.$touch" :error="validations.company.$error" :last="companyStep" />
+          @blur="validations.company.$touch" :error="validations.company.$error" :last="displayCompany" />
+      </template>
       <template slot="footer">
         <q-btn v-if="firstStep" no-caps class="full-width modal-btn" label="Suivant" color="primary"
           :loading="loading" icon-right="add" @click="nextStep" />
@@ -41,7 +41,7 @@ export default {
   props: {
     value: { type: Boolean, default: false },
     firstStep: { type: Boolean, default: true },
-    companyStep: { type: Boolean, default: false },
+    displayCompany: { type: Boolean, default: false },
     newUser: { type: Object, default: () => ({}) },
     companyOptions: { type: Array, default: () => [] },
     validations: { type: Object, default: () => ({}) },
