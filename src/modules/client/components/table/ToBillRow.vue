781d4a1eedc09c79dd61269c6616e0dfbbd61010
KennyCallegari <mel.kenny@gmail.com>
2021-09-16 16:50:01 +0200

    COM-2328 - Fix reviews

--- src/modules/client/components/table/ToBillRow.vue
+++ src/modules/client/components/table/ToBillRow.vue
@@ -26,12 +26,14 @@
         </div>
       </template>
       <template v-else-if="col.name === 'endDate'">{{ formatDate(bill.endDate) }}</template>
-      <template v-else-if="col.name === 'service' && !!bill.subscription">
-        {{ getLastVersion(bill.subscription.service.versions).name }}
+      <template v-else-if="col.name === 'service'">
+        <template v-if="!bill.subscription">{{ bill.billingItem.name }}</template>
+        <template v-else>{{ getLastVersion(bill.subscription.service.versions).name }}</template>
       </template>
-      <template v-else-if="col.name === 'service' && !bill.subscription">{{ bill.billingItem.name }}</template>
-      <template v-else-if="col.name === 'hours' && !!bill.subscription">{{ formatHours(bill) }}</template>
-      <template v-else-if="col.name === 'hours' && !bill.subscription">{{ bill.eventsList.length }}</template>
+      <template v-else-if="col.name === 'hours'">
+        <template v-if="!bill.subscription">{{ bill.eventsList.length }}</template>
+        <template v-else>{{ formatHours(bill) }}</template>
+      </template>
       <template v-else-if="col.name === 'unitExclTaxes'">{{ formatPrice(bill.unitExclTaxes) }}</template>
       <template v-else-if="col.name === 'discount'">
         <ni-editable-td :props="bill" edited-field="discount" edition-boolean-name="discountEdition"
