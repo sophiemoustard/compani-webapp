b84dc1d1a9977a8764e1478b0bf5f9d2f80ccbf8
CorentinMartinot <corentin.martinot@gmail.com>
2021-10-21 16:32:23 +0200

    COM-2421 create cmp button-toggle

---
+++ src/core/components/ButtonToggle.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-btn-toggle :class="displayClass" no-wrap :value="value" unelevated :toggle-color="toggleColor" :options="options"
+    @input="input" :text-color="textColor" />
+</template>
+
+<script>
+export default {
+  name: 'ButtonToggle',
+  props: {
+    toggleColor: { type: String, default: 'primary' },
+    textColor: { type: String, default: 'copper-grey-700' },
+    value: { type: String, default: '' },
+    displayClass: { type: String, default: '' },
+    options: { type: Array, default: () => [] },
+  },
+  methods: {
+    input (event) {
+      this.$emit('input', event);
+    },
+  },
+};
+</script>
+
+<style lang="stylus" scoped>
+  .q-btn-toggle
+    width: 100%
+    display: flex
+    @media screen and (max-width: 767px)
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+    >>> .q-btn-item
+      flex-grow: 1
+      flex-basis: 0
+      border-radius: 20px
+      margin: 5px
+      background-color: $copper-grey-300
+      > .q-btn__wrapper
+        padding: 0
+
+</style>
