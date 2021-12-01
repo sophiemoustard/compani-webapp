144bf797746eb6a7e16a3ffccbddf4d06008b677
ulysse <ulysse@alenvi.io>
2021-12-01 14:21:56 +0100

    COM-2518: allow link to company

---
+++ src/modules/vendor/components/companies/CompanyLinkModal.vue
@@ -0,0 +1,47 @@
+<template>
+  <ni-modal :value="value" @hide="hide" @input="input" container-class="modal-container-md">
+    <template slot="title">
+      Rattacher à une <span class="text-weight-bold">structure</span>
+    </template>
+    <ni-select in-modal :value="newCompany" @input="update" :error="validations.$error"
+      caption="Structure" :options="companyOptions" last required-field />
+    <template slot="footer">
+      <q-btn no-caps class="full-width modal-btn" label="Rattacher à la structure" color="primary" :loading="loading"
+        icon-right="add" @click="submit" />
+    </template>
+  </ni-modal>
+</template>
+
+<script>
+import Modal from '@components/modal/Modal';
+import Select from '@components/form/Select';
+
+export default {
+  name: 'CompanyLinkModal',
+  props: {
+    value: { type: Boolean, default: false },
+    validations: { type: Object, default: () => ({}) },
+    loading: { type: Boolean, default: false },
+    companyOptions: { type: Array, default: () => [] },
+    newCompany: { type: String, default: '' },
+  },
+  components: {
+    'ni-select': Select,
+    'ni-modal': Modal,
+  },
+  methods: {
+    hide () {
+      this.$emit('hide');
+    },
+    input (event) {
+      this.$emit('input', event);
+    },
+    submit () {
+      this.$emit('submit');
+    },
+    update (value) {
+      this.$emit('update:newCompany', value);
+    },
+  },
+};
+</script>
--- src/modules/vendor/pages/ni/users/learners/LearnerProfile.vue
+++ src/modules/vendor/pages/ni/users/learners/LearnerProfile.vue
@@ -1,21 +1,36 @@
 <template>
   <q-page padding class="vendor-background">
     <div v-if="userProfile">
-      <ni-profile-header :title="userIdentity" :header-info="headerInfo" />
+      <ni-profile-header :title="userIdentity" :header-info="headerInfo">
+        <template #title>
+          <ni-button color="primary" icon="add" v-if="!userProfile.company" class="q-ml-sm"
+            label="Rattacher à une structure" @click="openCompanyLinkModal" />
+        </template>
+      </ni-profile-header>
       <profile-tabs :profile-id="learnerId" :tabs-content="tabsContent" />
     </div>
+
+    <company-link-modal v-model="companyLinkModal" :loading="modalLoading" @submit="linkUserToCompany"
+      :validations="$v.newCompany" @hide="resetCompanyLinkModal" :new-company.sync="newCompany"
+      :company-options="companyOptions" />
   </q-page>
 </template>
 
 <script>
+import { required } from 'vuelidate/lib/validators';
 import { mapState } from 'vuex';
 import get from 'lodash/get';
+import Button from '@components/Button';
+import Companies from '@api/Companies';
+import Users from '@api/Users';
 import ProfileHeader from '@components/ProfileHeader';
 import ProfileTabs from '@components/ProfileTabs';
 import ProfileInfo from '@components/learners/ProfileInfo';
 import ProfileCourses from '@components/learners/ProfileCourses';
-import { formatIdentity } from '@helpers/utils';
+import { formatIdentity, formatAndSortOptions } from '@helpers/utils';
 import { learnerMixin } from '@mixins/learnerMixin';
+import { NotifyPositive, NotifyNegative, NotifyWarning } from '@components/popup/notify';
+import CompanyLinkModal from 'src/modules/vendor/components/companies/CompanyLinkModal';
 
 export default {
   name: 'LearnerProfile',
@@ -28,6 +43,8 @@ export default {
   components: {
     'ni-profile-header': ProfileHeader,
     'profile-tabs': ProfileTabs,
+    'ni-button': Button,
+    'company-link-modal': CompanyLinkModal,
   },
   data () {
     return {
@@ -36,11 +53,25 @@ export default {
         { label: 'Infos personnelles', name: 'info', default: this.defaultTab === 'info', component: ProfileInfo },
         { label: 'Formations', name: 'courses', default: this.defaultTab === 'courses', component: ProfileCourses },
       ],
+      companyOptions: [],
+      companyLinkModal: false,
+      newCompany: '',
+      modalLoading: false,
     };
   },
   async created () {
     await this.$store.dispatch('userProfile/fetchUserProfile', { userId: this.learnerId });
     this.userIdentity = formatIdentity(get(this, 'userProfile.identity'), 'FL');
+  },
+  watch: {
+    async userProfile () {
+      this.userIdentity = formatIdentity(get(this, 'userProfile.identity'), 'FL');
+    },
+  },
+  validations () {
+    return {
+      newCompany: { required },
+    };
   },
   computed: {
     ...mapState('userProfile', ['userProfile']),
@@ -54,9 +85,49 @@ export default {
       return infos;
     },
   },
-  watch: {
-    async userProfile () {
-      this.userIdentity = formatIdentity(get(this, 'userProfile.identity'), 'FL');
+  methods: {
+    async refreshUserProfile () {
+      try {
+        await this.$store.dispatch('userProfile/fetchUserProfile', { userId: this.userProfile._id });
+      } catch (e) {
+        console.error(e);
+      }
+    },
+    async openCompanyLinkModal () {
+      try {
+        const companies = await Companies.list();
+
+        this.companyOptions = formatAndSortOptions(companies, 'name');
+        this.companyLinkModal = true;
+      } catch (e) {
+        console.error(e);
+        this.companyLinkModal = false;
+        this.companyOptions = [];
+      }
+    },
+    resetCompanyLinkModal () {
+      this.companyOptions = [];
+      this.newCompany = '';
+      this.$v.newCompany.$reset();
+    },
+    async linkUserToCompany () {
+      try {
+        this.$v.newCompany.$touch();
+        if (this.$v.newCompany.$error) return NotifyWarning('Une structure est requise.');
+
+        this.modalLoading = true;
+        await Users.updateById(this.userProfile._id, { company: this.newCompany });
+
+        this.companyLinkModal = false;
+        NotifyPositive('Rattachement à la structure effectué.');
+
+        await this.refreshUserProfile();
+      } catch (e) {
+        console.error(e);
+        NotifyNegative('Erreur lors du rattachement à la structure.');
+      } finally {
+        this.modalLoading = false;
+      }
     },
   },
   beforeDestroy () {
