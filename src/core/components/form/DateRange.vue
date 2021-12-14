174398b00c1f21c0cd098e40b5b141801996d7f2
Manon Palin <manon@compani.fr>
2021-12-14 09:40:33 +0100

    COM-2552 - fix bug

--- src/core/components/form/DateRange.vue
+++ src/core/components/form/DateRange.vue
@@ -70,7 +70,7 @@ export default {
   methods: {
     update (value, key) {
       this.$v.value.$touch();
-      if (key === 'endDate') value = moment(value).endOf('d').toISOString();
+      if (key === 'endDate') value = moment(value).endOf('d').milliseconds(0).toISOString();
 
       this.$emit('input', { ...this.value, [key]: value });
     },
