97d6fb8570219cb2138462c4e91ff22f2470ab70
CorentinMartinot <corentin.martinot@gmail.com>
2022-01-28 17:43:01 +0100

    COM-2645-2 add creation modale

---
+++ src/modules/vendor/components/billing/CourseFundingOrganisationCreationModal.vue
@@ -0,0 +1,55 @@
+<template>
+  <ni-modal :model-value="modelValue" @hide="hide" @update:model-value="input" container-class="modal-container-md">
+    <template #title>
+      Ajouter un <span class="text-weight-bold">financeur</span>
+    </template>
+    <ni-input in-modal caption="Nom" :model-value="newOrganisation.name" @update:model-value="update($event, 'name')"
+      :error="validations.name.$error" required-field />
+    <ni-search-address in-modal last :model-value="newOrganisation.address" :error="validations.address.$error"
+      @update:model-value="update($event, 'address')" @blur="validations.address.$touch"
+      error-message="'Adresse non valide'" required-field />
+    <template #footer>
+      <ni-button class="full-width modal-btn bg-primary" label="Ajouter" icon-right="add" color="white"
+        :loading="loading" @click="submit" />
+    </template>
+  </ni-modal>
+</template>
+
+<script>
+import Modal from '@components/modal/Modal';
+import Input from '@components/form/Input';
+import Button from '@components/Button';
+import SearchAddress from '@components/form/SearchAddress';
+import set from 'lodash/set';
+
+export default {
+  name: 'CourseFundingOrganisationCreationModal',
+  props: {
+    modelValue: { type: Boolean, default: false },
+    newOrganisation: { type: Object, default: () => ({}) },
+    validations: { type: Object, default: () => ({}) },
+    loading: { type: Boolean, default: false },
+  },
+  components: {
+    'ni-search-address': SearchAddress,
+    'ni-modal': Modal,
+    'ni-input': Input,
+    'ni-button': Button,
+  },
+  emits: ['hide', 'update:model-value', 'submit', 'update:new-organisation'],
+  methods: {
+    hide () {
+      this.$emit('hide');
+    },
+    input (event) {
+      this.$emit('update:model-value', event);
+    },
+    submit () {
+      this.$emit('submit');
+    },
+    update (event, path) {
+      this.$emit('update:new-organisation', set({ ...this.newOrganisation }, path, event));
+    },
+  },
+};
+</script>
--- src/modules/vendor/pages/ni/billing/BillingConfig.vue
+++ src/modules/vendor/pages/ni/billing/BillingConfig.vue
@@ -1,28 +1,46 @@
 <template>
-  <q-page padding class="vendor-background q-pb-xl">
-    <ni-title-header title="Configuration de la facturation" class="q-mb-xl" />
-    <p class="text-weight-bold">Financeurs</p>
-    <q-card>
-      <ni-responsive-table :data="courseFundingOrganisations" :columns="courseFundingOrganisationColumns"
-        v-model:pagination="pagination" class="q-mb-md" :loading="tableLoading" />
-    </q-card>
-  </q-page>
+  <div>
+    <q-page padding class="vendor-background q-pb-xl">
+      <ni-title-header title="Configuration de la facturation" class="q-mb-xl" />
+      <p class="text-weight-bold">Financeurs</p>
+      <q-card>
+        <ni-responsive-table :data="courseFundingOrganisations" :columns="courseFundingOrganisationColumns"
+          v-model:pagination="pagination" class="q-mb-md" :loading="tableLoading" />
+        <q-card-actions align="right">
+          <ni-button color="primary" icon="add" label="Ajouter un financeur" :disable="tableLoading"
+            @click="openOrganisationCreationModal" />
+        </q-card-actions>
+      </q-card>
+    </q-page>
+
+    <ni-organisation-creation-modal v-model="organisationCreationModal" v-model:new-organisation="newOrganisation"
+      @submit="addOrganisation" :validations="validations.newOrganisation" @hide="resetOrganisationAdditionForm"
+      :loading="tableLoading" />
+  </div>
 </template>
 
 <script>
 import { useMeta } from 'quasar';
-import { ref } from 'vue';
-import { NotifyNegative } from '@components/popup/notify';
-import TitleHeader from '@components/TitleHeader';
-import CourseFundingOrganisations from '@api/CourseFundingOrganisations';
-import ResponsiveTable from '@components/table/ResponsiveTable';
+import { ref, computed } from 'vue';
+import useVuelidate from '@vuelidate/core';
+import { required } from '@vuelidate/validators';
 import get from 'lodash/get';
+import { frAddress } from '@helpers/vuelidateCustomVal';
+import { sortStrings } from '@helpers/utils';
+import CourseFundingOrganisations from '@api/CourseFundingOrganisations';
+import { NotifyNegative, NotifyPositive, NotifyWarning } from '@components/popup/notify';
+import TitleHeader from '@components/TitleHeader';
+import ResponsiveTable from '@components/table/ResponsiveTable';
+import Button from '@components/Button';
+import OrganisationCreationModal from 'src/modules/vendor/components/billing/CourseFundingOrganisationCreationModal';
 
 export default {
   name: 'BillingConfig',
   components: {
     'ni-title-header': TitleHeader,
     'ni-responsive-table': ResponsiveTable,
+    'ni-organisation-creation-modal': OrganisationCreationModal,
+    'ni-button': Button,
   },
   setup () {
     const metaInfo = { title: 'Configuration facturation' };
@@ -31,21 +49,63 @@ export default {
     const tableLoading = ref(false);
     const courseFundingOrganisations = ref([]);
     const courseFundingOrganisationColumns = [
-      { name: 'type', label: 'Nom', align: 'left', field: 'name' },
+      { name: 'name', label: 'Nom', align: 'left', field: 'name' },
       { name: 'address', label: 'Adresse', align: 'left', field: row => get(row, 'address.fullAddress') || '' },
     ];
     const pagination = { rowsPerPage: 0 };
+    const organisationCreationModal = ref(false);
+    const newOrganisation = ref({ name: '', address: {} });
+    const modalLoading = ref(false);
+
+    const rules = computed(() => ({
+      newOrganisation: {
+        address: {
+          zipCode: { required },
+          street: { required },
+          city: { required },
+          fullAddress: { frAddress },
+        },
+        name: { required },
+      },
+    }));
+    const validations = useVuelidate(rules, { newOrganisation });
 
     const refreshCourseFundingOrganisations = async () => {
       try {
         tableLoading.value = true;
-        courseFundingOrganisations.value = await CourseFundingOrganisations.list();
+        const organisations = await CourseFundingOrganisations.list();
+        courseFundingOrganisations.value = organisations.sort((a, b) => sortStrings(a.name, b.name));
       } catch (e) {
         console.error(e);
         courseFundingOrganisations.value = [];
         NotifyNegative('Erreur lors de la récupération des financeurs.');
       } finally {
         tableLoading.value = false;
+      }
+    };
+
+    const openOrganisationCreationModal = () => { organisationCreationModal.value = true; };
+    const resetOrganisationAdditionForm = () => {
+      newOrganisation.value = { name: '', address: {} };
+      validations.value.newOrganisation.$reset();
+    };
+    const addOrganisation = async () => {
+      try {
+        validations.value.newOrganisation.$touch();
+        if (validations.value.newOrganisation.$error) return NotifyWarning('Champ(s) invalide(s)');
+
+        modalLoading.value = true;
+        await CourseFundingOrganisations.create(newOrganisation.value);
+        NotifyPositive('Financeur ajouté.');
+
+        organisationCreationModal.value = false;
+        await refreshCourseFundingOrganisations();
+      } catch (e) {
+        console.error(e);
+        if (e.status === 409) return NotifyNegative(e.data.message);
+        NotifyNegative('Erreur lors de l\'ajout du financeur.');
+      } finally {
+        modalLoading.value = false;
       }
     };
 
@@ -61,8 +121,15 @@ export default {
       tableLoading,
       pagination,
       courseFundingOrganisations,
+      organisationCreationModal,
+      newOrganisation,
+      // Computed
+      validations,
       // Methods
       refreshCourseFundingOrganisations,
+      resetOrganisationAdditionForm,
+      addOrganisation,
+      openOrganisationCreationModal,
     };
   },
 };
