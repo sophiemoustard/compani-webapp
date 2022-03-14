2b22b46ad125785b761aea2edfac77547d41e41b
Manon Palin <manon@compani.fr>
2022-03-14 16:24:57 +0100

    COM-2742 - add billingItems rate in weeklyRate calculation

--- src/modules/client/mixins/subscriptionMixin.js
+++ src/modules/client/mixins/subscriptionMixin.js
@@ -82,6 +82,8 @@ export const subscriptionMixin = {
         const totalBillingItemsAmount = subscription.service.billingItems
           .reduce((acc, bi) => (acc += bi.defaultUnitAmount), 0);
         weeklyRate += totalBillingItemsAmount * subscription.weeklyCount;
+      } else if (get(subscription, 'billingItemsTTCRate') && get(subscription, 'weeklyCount')) {
+        weeklyRate += subscription.weeklyCount * subscription.billingItemsTTCRate;
       }
 
       let fundingReduction = 0;
