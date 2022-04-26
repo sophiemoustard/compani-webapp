53592526f0f0d79dbfa758f4e564de15140eb748
Manon Palin <manon@compani.fr>
2022-04-26 12:20:04 +0200

    COM-2808 - uses cents to calculate

--- src/core/helpers/utils.js
+++ src/core/helpers/utils.js
@@ -76,6 +76,13 @@ export const roundFrenchPercentage = (number, digits = 2) => (number
 
 export const formatPrice = val => (val ? roundFrenchPrice(val) : roundFrenchPrice(0));
 
+export const formatStringToPrice = (str) => {
+  const slicedStr = str.slice(0, 6);
+  const price = formatPrice(parseFloat(slicedStr));
+
+  return price;
+};
+
 export const formatPriceWithSign = value => (value >= 0 ? `+${formatPrice(value)}` : formatPrice(value));
 
 export const formatIdentity = (identity, format) => {
@@ -153,3 +160,7 @@ export const getBillingItemsName = (service) => {
 };
 
 export const formatDownloadName = name => name.replaceAll(/ - | |'/g, '_');
+
+export const toCents = value => parseFloat(value).toFixed(2) * 100;
+
+export const toEuros = value => value / 100;
