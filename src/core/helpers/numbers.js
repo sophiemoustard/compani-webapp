5025c841dde28e18dcc067e0b128469d1b43e4dd
Sophie Moustard <sophiemoustard@sophies-macbook-pro.home>
2022-05-05 10:55:55 +0200

    COM-2832 - use BN to calculate

--- src/core/helpers/numbers.js
+++ src/core/helpers/numbers.js
@@ -1,5 +1,19 @@
 const { BigNumber } = require('bignumber.js');
 
+exports.toString = a => BigNumber(a).toString();
+
+exports.toFixed = (a, decimalPlaces = 2) => parseFloat(BigNumber(a).toFixed(decimalPlaces));
+
+exports.multiply = (...nums) => nums.reduce((acc, n) => BigNumber(acc).multipliedBy(n).toString(), 1);
+
 exports.divide = (a, b) => BigNumber(a).dividedBy(b).toString();
 
 exports.add = (...nums) => nums.reduce((acc, n) => BigNumber(acc).plus(n).toString(), 0);
+
+exports.subtract = (a, b) => BigNumber(a).minus(b).toString();
+
+exports.isGreaterThan = (a, b) => BigNumber(a).isGreaterThan(b);
+
+exports.isLessThan = (a, b) => BigNumber(a).isLessThan(b);
+
+exports.isEqualTo = (a, b) => BigNumber(a).isEqualTo(b);
