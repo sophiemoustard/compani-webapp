13db386c2e555c13b7eb802c2455bff436286962
Manon Palin <manon@compani.fr>
2022-05-25 16:35:45 +0200

    COM-2861 - fix Romain's reviews

--- src/modules/client/pages/ni/config/RhConfig.vue
+++ src/modules/client/pages/ni/config/RhConfig.vue
@@ -33,8 +33,9 @@
             v-model="company.rhConfig.grossHourlyRate" @focus="saveTmp('rhConfig.grossHourlyRate')" suffix="€"
             @blur="updateCompany('rhConfig.grossHourlyRate')" />
           <div class="col-xs-12 col-md-6">
-              <q-checkbox v-model="company.rhConfig.shouldPayHolidays" label="Les jours fériés ne sont pas travaillés"
-                @update:model-value="updateCompany('rhConfig.shouldPayHolidays')" dense class="title" />
+              <q-checkbox v-model="company.rhConfig.shouldPayHolidays" dense class="q-mr-sm"
+                @update:model-value="updateCompany('rhConfig.shouldPayHolidays')" />
+              <span>Les jours fériés ne sont pas travaillés</span>
               <div class="text">
                 Si vous cochez cette option, les jours fériés ne seront pas considérés comme des jours à travailler,
                 cela impactera les compteurs d'heures et la paie
@@ -540,10 +541,6 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-.title
-  font-size: 16px
-  font-weight: bold
-  color: $copper-grey-900
 .text
   font-size: 12px
   padding-left: 32px
