979a5032cb6fe3df276f0b058b54c61fafb71ca2
Manon Palin <manon@compani.fr>
2022-06-14 15:02:12 +0200

    COM-2871 - use ni-modal

--- src/modules/client/components/planning/EventCancellationModal.vue
+++ src/modules/client/components/planning/EventCancellationModal.vue
@@ -1,25 +1,20 @@
 <template>
-  <q-dialog :model-value="modelValue" @hide="hide" @update:model-value="$emit('update:model-value')">
-    <div class="modal-container-sm modal-padding in-modal">
-      <div class="row justify-between">
-        <div class="title q-mb-md">Annuler l'intervention</div>
-        <q-icon class="cursor-pointer" name="close" size="1.5rem" @close="resetForm" v-close-popup />
-      </div>
-      <ni-option-group caption="Qui est à l'origine de l'annulation ?"
-        :model-value="editedEvent.cancel.reason" type="radio" :options="cancellationReasons" required-field
-        color="copper-500" @update:model-value="updateCancellationReason($event)" />
-      <ni-option-group caption="Quelles sont les conditions d'annulation ?"
-        :model-value="editedEvent.cancel.condition" type="radio" :options="cancellationOptions" required-field
-        color="copper-500" @update:model-value="updateCancellationCondition($event)" />
-      <ni-input in-modal type="textarea" :model-value="editedEvent.misc" caption="Notes" required-field last
-        @update:model-value="updateEventMisc($event)" @blur="validations.misc.$touch"
-        :error="validations.misc.$error" />
-      <div class="row justify-end q-mb-md">
-        <ni-button label="RETOUR" @click="resetForm" />
-        <ni-button label="ANNULER L'INTERVENTION" @click="canCancelEvent" />
-      </div>
+  <ni-modal :model-value="modelValue" @hide="hide" @update:model-value="$emit('update:model-value')"
+    title="Annuler l'intervention">
+    <ni-option-group caption="Qui est à l'origine de l'annulation ?"
+      :model-value="editedEvent.cancel.reason" type="radio" :options="cancellationReasons" required-field
+      color="copper-500" @update:model-value="updateCancellationReason($event)" />
+    <ni-option-group caption="Quelles sont les conditions d'annulation ?"
+      :model-value="editedEvent.cancel.condition" type="radio" :options="cancellationOptions" required-field
+      color="copper-500" @update:model-value="updateCancellationCondition($event)" />
+    <ni-input in-modal type="textarea" :model-value="editedEvent.misc" caption="Notes" required-field last
+      @update:model-value="updateEventMisc($event)" @blur="validations.misc.$touch"
+      :error="validations.misc.$error" />
+    <div class="row justify-end q-mb-md">
+      <ni-button label="RETOUR" @click="resetForm" />
+      <ni-button label="ANNULER L'INTERVENTION" @click="canCancelEvent" />
     </div>
-  </q-dialog>
+  </ni-modal>
 </template>
 <script>
 
@@ -31,6 +26,7 @@ import { NotifyPositive, NotifyWarning } from '@components/popup/notify';
 import Button from '@components/Button';
 import Input from '@components/form/Input';
 import OptionGroup from '@components/form/OptionGroup';
+import Modal from '@components/modal/Modal';
 
 export default {
   name: 'EventCancellationModal',
@@ -38,6 +34,7 @@ export default {
     'ni-button': Button,
     'ni-input': Input,
     'ni-option-group': OptionGroup,
+    'ni-modal': Modal,
   },
   props: {
     modelValue: { type: Boolean, default: false },
