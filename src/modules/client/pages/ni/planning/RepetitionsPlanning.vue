9e908a62f87531577b2b78ebd482fcbeb8676502
Manon Palin <manon@compani.fr>
2022-06-23 19:19:15 +0200

    COM-2898 - add repetition container and title

--- src/modules/client/pages/ni/planning/RepetitionsPlanning.vue
+++ src/modules/client/pages/ni/planning/RepetitionsPlanning.vue
@@ -1,13 +1,17 @@
 <template>
   <q-page class="client-background" padding>
     <ni-title-header title="Gérer les répétitions" />
-    <ni-select caption="Auxiliaires" :model-value="selectedAuxiliary" :options="activeAuxiliaries" 
+    <ni-select caption="Auxiliaires" :model-value="selectedAuxiliary" :options="activeAuxiliaries"
       @update:model-value="setAuxiliary($event)" clearable class="q-mt-xl" />
+    <q-card v-if="selectedAuxiliary" class="cell-container">
+      <div class="cell-title">Répétitions de  <span class="text-weight-bold">{{ getAuxiliaryName() }}</span></div>
+    </q-card>
   </q-page>
 </template>
 
 <script>
 import { ref } from 'vue';
+import { get } from 'lodash';
 import Users from '@api/Users';
 import { formatAndSortIdentityOptions } from '@helpers/utils';
 import TitleHeader from '@components/TitleHeader';
@@ -39,6 +43,11 @@ export default {
       activeAuxiliaries.value = aux.flat();
     };
 
+    const getAuxiliaryName = () => {
+      const auxiliaryName = activeAuxiliaries.value.find(aux => get(aux, 'value') === selectedAuxiliary.value);
+      return get(auxiliaryName, 'label');
+    };
+
     created();
 
     return {
@@ -48,7 +57,16 @@ export default {
       // Methods
       getActiveAuxiliaries,
       setAuxiliary,
+      getAuxiliaryName,
     };
   },
 };
 </script>
+<style lang="sass" scoped>
+.cell-container
+  display: flex
+  flex-direction: column
+  padding: 8px 16px
+.cell-title
+  font-size: 20px
+</style>
