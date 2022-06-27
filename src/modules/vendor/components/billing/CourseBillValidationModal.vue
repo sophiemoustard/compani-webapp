aaa4097b1d2409db38213fde092feccd22cb52c4
ulysse <ulysse@alenvi.io>
2022-06-27 12:37:40 +0200

    COM-2902: use banner

--- src/modules/vendor/components/billing/CourseBillValidationModal.vue
+++ src/modules/vendor/components/billing/CourseBillValidationModal.vue
@@ -4,10 +4,10 @@
       Confirmation
     </template>
     <div class="course-name">{{ getCourseName }} </div>
-    <div v-if="!traineesLength && course.type === INTER_B2B" class="banner row q-pa-sm q-mb-md">
-      <q-icon size="sm" name="info_outline" color="orange-700" class="q-mr-sm" />
-      <div>Aucun stagiaire de la structure n'est inscrit à la formation</div>
-    </div>
+    <ni-banner v-if="!traineesLength && course.type === INTER_B2B" icon="info_outline" icon-color="orange-700"
+      class="bg-orange-50 text-orange-900">
+      <template #message>Aucun stagiaire de la structure n'est inscrit à la formation</template>
+    </ni-banner>
    <ni-date-input caption="Date de facture" :model-value="billToValidate.billedAt" :error="validations.billedAt.$error"
       @blur="validations.billedAt.$touch" in-modal required-field @update:model-value="update($event, 'billedAt')" />
     <template #footer>
@@ -25,6 +25,7 @@ import { toRefs, computed } from 'vue';
 import get from 'lodash/get';
 import set from 'lodash/set';
 import Modal from '@components/modal/Modal';
+import Banner from '@components/Banner';
 import Button from '@components/Button';
 import DateInput from '@components/form/DateInput';
 import { INTER_B2B } from '@data/constants';
@@ -44,6 +45,7 @@ export default {
     'ni-modal': Modal,
     'ni-button': Button,
     'ni-date-input': DateInput,
+    'ni-banner': Banner,
   },
   emits: ['hide', 'update:model-value', 'submit', 'cancel', 'update:bill-to-validate'],
   setup (props, { emit }) {
