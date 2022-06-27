3545ac24feb91a0b27504c95c0860d9d407c2e47
Chloe Molle <chloe.molle@neuf.fr>
2022-06-27 15:15:33 +0200

    COM-2888 all dates in utc

--- test/cypress/support/utils.js
+++ test/cypress/support/utils.js
@@ -1,7 +1,6 @@
 const moment = require('../../../src/core/helpers/moment');
 
 exports.applyOffset = (hours, minutes) => {
-  const parisOffset = moment.tz('Europe/Paris')._offset;
   const utcOffset = moment().utcOffset();
-  return `${hours + (utcOffset - parisOffset) / 60}:${minutes}`;
+  return `${hours + utcOffset / 60}:${minutes}`;
 };
