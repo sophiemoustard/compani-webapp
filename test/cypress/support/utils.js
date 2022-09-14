8a26fb06b0282d21aa4f6e075e81f734e35a9787
Palin Manon <manon@compani.fr>
2022-09-14 11:28:37 +0200

    COM-2990 - remove modifications on test e2e

--- test/cypress/support/utils.js
+++ test/cypress/support/utils.js
@@ -1,7 +1,6 @@
-const { DateTime } = require('../../../src/core/helpers/dates/luxon');
+const moment = require('../../../src/core/helpers/moment');
 
 exports.applyOffset = (hours, minutes) => {
-  const utcOffset = DateTime.now().offset;
-
+  const utcOffset = moment().utcOffset();
   return `${hours + utcOffset / 60}:${minutes}`;
 };
