8c6e68e5b3af51a1bf691156b142aa83c4811b9c
Chloe Molle <chloe.molle@neuf.fr>
2022-09-20 06:45:01 -0600

    COM-3006 remove luxon timezone

--- src/core/helpers/dates/luxon.js
+++ src/core/helpers/dates/luxon.js
@@ -1,7 +1,6 @@
 import * as luxon from 'luxon';
 
 luxon.Settings.defaultLocale = 'fr';
-luxon.Settings.defaultZone = 'Europe/Paris';
 luxon.Settings.throwOnInvalid = true;
 
 export { DateTime } from 'luxon';
