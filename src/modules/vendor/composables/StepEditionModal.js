76a091d46c050707cf531164182d969b21d5f2cd
ulysse <ulysse@alenvi.io>
2022-10-28 14:13:20 +0200

    COM-3250: proposition 1

--- src/modules/vendor/composables/StepEditionModal.js
+++ src/modules/vendor/composables/StepEditionModal.js
@@ -1,12 +1,11 @@
 import { ref, computed } from 'vue';
 import useVuelidate from '@vuelidate/core';
 import pick from 'lodash/pick';
-import { getHoursAndMinutes, computeHours } from '@helpers/date';
-import { integerNumber, positiveNumber } from '@helpers/vuelidateCustomVal';
 import { E_LEARNING, REQUIRED_LABEL } from '@data/constants';
-import { required, requiredIf, maxValue } from '@vuelidate/validators';
+import { required } from '@vuelidate/validators';
 import Steps from '@api/Steps';
 import { NotifyNegative, NotifyWarning, NotifyPositive } from '@components/popup/notify';
+import CompaniDuration from '@helpers/dates/companiDurations';
 
 export const useStepEditionModal = (
   isLocked,
@@ -15,20 +14,27 @@ export const useStepEditionModal = (
   modalLoading,
   openNextModalAfterUnlocking
 ) => {
-  const editedStep = ref({ name: '', type: E_LEARNING, theoreticalHours: { hours: 0, minutes: 0 } });
+  const editedStep = ref({ name: '', type: E_LEARNING, theoreticalDuration: { hours: 'PT0H', minutes: 'PT0M' } });
   const stepEditionModal = ref(false);
+
+  const validateISOHours = (value) => {
+    if (!value) return true;
+
+    return editedStep.value.theoreticalDuration.minutes === 'PT0M'
+      ? !!value.match(/PT[1-9]\d*H/)
+      : !!value.match(/PT[0-9]\d*H/);
+  };
+
+  const validateISOMinutes = (value) => {
+    if (!value) return true;
+    return editedStep.value.theoreticalDuration.hours === 'PT0H'
+      ? !!value.match(/PT([1-9]|[1-5]\d)M/)
+      : !!value.match(/PT(\d|[1-5]\d)M/);
+  };
 
   const rules = computed(() => ({
     name: { required },
-    theoreticalHours: {
-      hours: { required: requiredIf(!editedStep.value.theoreticalHours.minutes), integerNumber, positiveNumber },
-      minutes: {
-        required: requiredIf(!editedStep.value.theoreticalHours.hours),
-        integerNumber,
-        positiveNumber,
-        maxValue: maxValue(59),
-      },
-    },
+    theoreticalDuration: { hours: { required, validateISOHours }, minutes: { required, validateISOMinutes } },
   }));
   const v$ = useVuelidate(rules, editedStep);
 
@@ -37,9 +43,10 @@ export const useStepEditionModal = (
       openNextModalAfterUnlocking.value = () => openStepEditionModal(step);
       openValidateUnlockingEditionModal(step);
     } else {
+      const { hours, minutes } = CompaniDuration(step.theoreticalDuration || 'PT0S').toHoursAndMinutesObject();
       editedStep.value = {
         ...pick(step, ['_id', 'name', 'type']),
-        theoreticalHours: getHoursAndMinutes(step.theoreticalHours),
+        theoreticalDuration: { hours: `PT${hours}H`, minutes: `PT${minutes}M` },
       };
       stepEditionModal.value = true;
     }
@@ -50,11 +57,10 @@ export const useStepEditionModal = (
       modalLoading.value = true;
       v$.value.$touch();
       if (v$.value.$error) return NotifyWarning('Champ(s) invalide(s)');
-
-      await Steps.updateById(
-        editedStep.value._id,
-        { ...pick(editedStep.value, ['name']), theoreticalHours: computeHours(editedStep.value.theoreticalHours) }
-      );
+      const payload = { ...pick(editedStep.value, ['name']),
+        theoreticalDuration: CompaniDuration(editedStep.value.theoreticalDuration.hours)
+          .add(editedStep.value.theoreticalDuration.minutes).toISO() };
+      await Steps.updateById(editedStep.value._id, payload);
       stepEditionModal.value = false;
       await refreshProgram();
       NotifyPositive('Étape modifiée.');
@@ -67,23 +73,20 @@ export const useStepEditionModal = (
   };
 
   const resetStepEditionModal = () => {
-    editedStep.value = { name: '', theoreticalHours: { hours: 0, minutes: 0 } };
+    editedStep.value = { name: '', theoreticalDuration: { hours: 'PT0H', minutes: 'PT0M' } };
     v$.value.$reset();
   };
 
   const theoreticalHoursErrorMsg = computed(() => {
-    if (!v$.value.theoreticalHours.hours.required.$response) return REQUIRED_LABEL;
-    if (!v$.value.theoreticalHours.hours.integerNumber.$response ||
-      !v$.value.theoreticalHours.hours.positiveNumber.$response) return 'Durée non valide';
+    if (!v$.value.theoreticalDuration.hours.required.$response) return REQUIRED_LABEL;
+    if (!v$.value.theoreticalDuration.hours.validateISOHours.$response) return 'Durée non valide';
 
     return '';
   });
 
   const theoreticalMinutesErrorMsg = computed(() => {
-    if (!v$.value.theoreticalHours.minutes.required.$response) return REQUIRED_LABEL;
-    if (!v$.value.theoreticalHours.minutes.integerNumber.$response ||
-     !v$.value.theoreticalHours.minutes.positiveNumber.$response ||
-     !v$.value.theoreticalHours.minutes.maxValue.$response) return 'Durée non valide';
+    if (!v$.value.theoreticalDuration.minutes.required.$response) return REQUIRED_LABEL;
+    if (!v$.value.theoreticalDuration.minutes.validateISOMinutes.$response) return 'Durée non valide';
 
     return '';
   });
