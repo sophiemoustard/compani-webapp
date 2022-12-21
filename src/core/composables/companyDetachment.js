5e1bac1b11a78624526e62eb0c1b949ae43c9f49
ulysse <ulysse@alenvi.io>
2022-12-21 15:45:59 +0100

    COM-3336: bloc detachment if not yet link

--- src/core/composables/companyDetachment.js
+++ src/core/composables/companyDetachment.js
@@ -20,8 +20,15 @@ export const useCompanyDetachment = (userProfile, refresh) => {
   const userIdentity = ref('');
   const isClientInterface = !/\/ad\//.test($router.currentRoute.value.path);
 
-  const detachableUserCompany = computed(() => (get(userProfile.value, 'userCompanyList') || [])
-    .find(uc => !uc.endDate && CompaniDate().isAfter(uc.startDate)));
+  const currentOrFutureUserCompany = computed(() => (get(userProfile.value, 'userCompanyList') || [])
+    .find(uc => !uc.endDate));
+
+  const detachableUserCompany = computed(() => (
+    get(currentOrFutureUserCompany.value, 'startDate') &&
+    CompaniDate().isAfter(currentOrFutureUserCompany.value.startDate)
+      ? currentOrFutureUserCompany.value
+      : undefined
+  ));
 
   const minDetachmentDate = computed(() => (get(detachableUserCompany.value, 'startDate') || ''));
 
@@ -84,6 +91,7 @@ export const useCompanyDetachment = (userProfile, refresh) => {
     // Computed
     companyName,
     canDetachFromCompany,
+    currentOrFutureUserCompany,
     minDetachmentDate,
     // Methods
     openCompanyDetachModal,
