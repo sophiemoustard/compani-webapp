a330fd2a1685a973d29b6427a078c9c5514c75fd
Palin Manon <manon@compani.fr>
2023-07-06 14:41:42 +0200

    COM-3496 - renaming ContactProfile title

--- src/modules/client/pages/ni/courses/ContactProfile.vue
+++ src/modules/client/pages/ni/courses/ContactProfile.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page padding class="client-background">
-    <ni-title-header title="Vos contacts Compani" class="q-mb-lg" />
+    <ni-title-header title="Mes contacts Compani" class="q-mb-lg" />
     <span class="text-italic">
       Des questions sur l'organisation d'une formation ? Retrouvez le contact sur l’onglet “Organisation”
       de la formation.
