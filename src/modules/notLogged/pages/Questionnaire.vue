4c40490d4225c5219d720435af90e591159ac36f
Palin Manon <manon@compani.fr>
2023-08-16 18:01:38 +0200

    COM-3522 - add missing file

---
+++ src/modules/notLogged/pages/Questionnaire.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <compani-header />
+    <div class="row justify-center client-background">
+      Merci ! A tres vite pour une formation
+    </div>
+  </div>
+</template>
+
+<script>
+import CompaniHeader from '@components/CompaniHeader';
+
+export default {
+  name: 'Questionnaire',
+  props: {
+    questionnaireId: { type: String, required: true },
+    courseId: { type: String, required: true },
+  },
+  components: {
+    'compani-header': CompaniHeader,
+  },
+  setup () {
+    return {
+    };
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.client-background
+  min-height: 100vh
+  @media screen and (max-width: 768px)
+    height: 85vh
+</style>
