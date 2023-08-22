ebadf3987bf28781ab23a88eba338198ad56ebce
Palin Manon <manon@compani.fr>
2023-08-22 17:29:43 +0200

    COM-3523 - can create Questionnnaire histories

--- src/modules/notLogged/pages/QuestionnaireForm.vue
+++ src/modules/notLogged/pages/QuestionnaireForm.vue
@@ -6,7 +6,7 @@
       <start v-if="cardIndex === -1" :course="course" :trainee="tmpTrainee" @update-trainee="updateTrainee"
         @click="updateCardIndex" />
       <end v-if="cardIndex === questionnaire.cards.length" :course="course" :trainee="tmpTrainee"
-        @click="updateCardIndex" />
+        @click="updateCardIndex" @submit="createHistory" />
     </div>
   </div>
 </template>
@@ -22,7 +22,9 @@ import CompaniHeader from '@components/CompaniHeader';
 import MetaInfos from '@components/questionnaires/cards/MetaInfos';
 import Start from '@components/questionnaires/cards/Start';
 import End from '@components/questionnaires/cards/End';
+import { NotifyNegative, NotifyPositive } from '@components/popup/notify';
 import { INCREMENT } from '@data/constants';
+import QuestionnaireHistories from '@api/QuestionnaireHistories';
 
 export default {
   name: 'QuestionnaireForm',
@@ -69,6 +71,22 @@ export default {
 
     const updateCardIndex = async type => $store.dispatch('questionnaire/updateCardIndex', { type });
 
+    const createHistory = async () => {
+      try {
+        const payload = {
+          course: course.value._id,
+          questionnaire: questionnaire.value._id,
+          user: tmpTrainee.value._id,
+        };
+
+        await QuestionnaireHistories.create(payload);
+        NotifyPositive('Réponse enregistrée.');
+      } catch (e) {
+        console.error(e);
+        NotifyNegative('Erreur lors de l\'enregistrement des réponses au questionnaire.');
+      }
+    };
+
     return {
       // Data
       course,
@@ -80,6 +98,7 @@ export default {
       // Methods
       updateTrainee,
       updateCardIndex,
+      createHistory,
     };
   },
 };
