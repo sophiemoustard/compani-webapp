852ea4451f8e3000d9a60071662dde98c29b104b
Palin Manon <manon@compani.fr>
2023-08-24 11:19:20 +0200

    COM-3523 - creater footer componennt  + fix style

--- src/core/components/questionnaires/cards/End.vue
+++ src/core/components/questionnaires/cards/End.vue
@@ -1,46 +1,28 @@
 <template>
-  <div class="card-container">
-    <ni-button class="back-btn" icon="arrow_back" color="primary" @click="updateCardIndex(DECREMENT)" />
-    <ni-button class="bg-primary btn" label="Valider" color="white" :loading="loading" @click="submit" />
-  </div>
+    <ni-footer label="Valider" @submit="submit" :loading="loading" />
 </template>
 
 <script>
-import get from 'lodash/get';
-import { toRefs, computed } from 'vue';
-import { formatAndSortUserOptions } from '@helpers/utils';
-import Button from '@components/Button';
+import Footer from '@components/questionnaires/cards/Footer';
 import { DECREMENT } from '@data/constants';
 
 export default {
   name: 'Start',
   components: {
-    'ni-button': Button,
+    'ni-footer': Footer,
   },
   props: {
-    course: { type: Object, required: true },
-    trainee: { type: Object, required: true },
     loading: { type: Boolean, required: true },
   },
-  emits: ['update-trainee', 'click', 'submit'],
-  setup (props, { emit }) {
-    const { course } = toRefs(props);
-    const traineesOptions = computed(() => (get(course.value, 'trainees')
-      ? formatAndSortUserOptions(course.value.trainees, false)
-      : []));
-
-    const updateCardIndex = type => emit('click', type);
-
+  emits: ['submit'],
+  setup (_, { emit }) {
     const submit = () => emit('submit');
 
     return {
       // DATA
       DECREMENT,
       // Methods
-      updateCardIndex,
       submit,
-      // Computed
-      traineesOptions,
     };
   },
 };
