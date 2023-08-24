bc91708978065e99aeec25521568997664bd3070
Palin Manon <manon@compani.fr>
2023-08-24 09:40:22 +0200

    COM-3523 - fix background color problem

--- src/modules/notLogged/pages/QuestionnaireForm.vue
+++ src/modules/notLogged/pages/QuestionnaireForm.vue
@@ -119,8 +119,6 @@ export default {
 
 <style lang="sass" scoped>
 .client-background
-  min-height: 90vh
-  @media screen and (max-width: 768px)
-    height: 85vh
+  height: 100vh
   padding: 48px
 </style>
