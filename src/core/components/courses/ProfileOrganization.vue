b40ebb31e9c6cb3854d26bd77daf7c68c0d6b256
ulysse <ulysse@alenvi.io>
2023-09-06 16:38:59 +0200

    COM-3539: rename variable

--- src/core/components/courses/ProfileOrganization.vue
+++ src/core/components/courses/ProfileOrganization.vue
@@ -260,7 +260,7 @@ export default {
 
     const v$ = useVuelidate(rules, { tmpInterlocutor, tmpContactId, tmpCourse, newSms });
 
-    const isTrainer = computed(() => vendorRole.value === TRAINER);
+    const loggedUserIsTrainer = computed(() => vendorRole.value === TRAINER);
 
     const isRofOrVendorAdmin = computed(() => [VENDOR_ADMIN, TRAINING_ORGANISATION_MANAGER].includes(vendorRole.value));
 
@@ -440,12 +440,12 @@ export default {
         const loggedUserCompany = get(loggedUser.value, 'company._id');
         const courseCompany = course.value.type === INTRA ? course.value.companies[0]._id : '';
 
-        if (isTrainer.value && loggedUserCompany !== courseCompany) {
+        if (loggedUserIsTrainer.value && loggedUserCompany !== courseCompany) {
           companyRepresentativeOptions.value = [];
           return;
         }
         const clientsUsersAllowedtoAccessCompany = course.value.type === INTRA
-          ? await Users.list({ role: [COACH, CLIENT_ADMIN], company: courseCompany, includesHoldingAdmins: true })
+          ? await Users.list({ role: [COACH, CLIENT_ADMIN], company: courseCompany, includeHoldingAdmins: true })
           : [];
 
         companyRepresentativeOptions.value = Object.freeze(clientsUsersAllowedtoAccessCompany
