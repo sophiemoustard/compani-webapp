f2f7ab6dc0e31b3a2818759276298838581facfa
Manon Palin <manonpalin@mbp-de-manon.home>
2023-09-18 18:00:46 +0200

    COM-3528 - can answer to QuestionAnswer component

--- src/core/components/questionnaires/cards/QuestionAnswer.vue
+++ src/core/components/questionnaires/cards/QuestionAnswer.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>Question reponses</div>
-  <ni-footer label="Suivant" @submit="updateCardIndex" />
+  <div class="card elm-width">
+    <p class="title">
+      {{ card.question }}
+      <span v-if="card.isQuestionAnswerMultipleChoiced"> ( plusieurs réponses sont attendues ) </span>
+    </p>
+    <div v-for="(answer, index) of card.qcAnswers" :key="answer._id" class="answers">
+      <q-checkbox :model-value="isAnswerSelected[index]" @update:model-value="updateSelectedAnswer(index)"
+        :label="answer.text" />
+    </div>
+    <ni-footer label="Suivant" @submit="updateQuestionnaireAnswer" />
+  </div>
+  {{ card }}
 </template>
 
 <script>
 import { useStore } from 'vuex';
+import { ref, toRefs, computed } from 'vue';
 import { INCREMENT } from '../../../data/constants';
 import Footer from './Footer';
 
@@ -16,14 +27,59 @@ export default {
   props: {
     card: { type: Object, required: true },
   },
-  setup () {
+  setup (props) {
+    const { card } = toRefs(props);
     const $store = useStore();
-    const updateCardIndex = () => $store.dispatch('questionnaire/updateCardIndex', { type: INCREMENT });
+    const isAnswerSelected = ref(new Array(card.value.qcAnswers.length).fill(false));
 
+    const updateSelectedAnswer = (index) => {
+      isAnswerSelected.value[index] = !isAnswerSelected.value[index];
+    };
+
+    const updateQuestionnaireAnswer = () => {
+      const answerSelectedIndexes = [...isAnswerSelected.value.keys()].filter(i => isAnswerSelected.value[i]);
+      const answer = [];
+      answerSelectedIndexes.forEach(idx => answer.push(card.value.qcAnswers[idx]._id));
+
+      $store.dispatch(
+        'questionnaire/setAnswerList',
+        { answers: [{ card: card.value._id, answerList: [...answer] }] }
+      );
+
+      $store.dispatch('questionnaire/updateCardIndex', { type: INCREMENT });
+    };
+
+    const answerList = computed(() => $store.state.questionnaire.answerList);
+
+    const created = () => {
+      const initialValue = answerList.value.find(a => a.card === card.value._id);
+
+      if (initialValue) {
+        for (const answer of initialValue.answerList) {
+          const index = card.value.qcAnswers.findIndex(a => a._id === answer);
+          isAnswerSelected.value[index] = true;
+        }
+      }
+    };
+
+    created();
     return {
+      // Data
+      isAnswerSelected,
       // Methods
-      updateCardIndex,
+      updateSelectedAnswer,
+      updateQuestionnaireAnswer,
     };
   },
 };
 </script>
+<style lang="sass" scoped>
+.container
+.title
+  color: $copper-grey-500
+  font-size: 14px
+.answers
+  display: flex
+  flex-direction: column
+  padding: 0 0 0 4px
+</style>
