5d19beb1b30f94dab3b24d577253fdea427f2e17
ulysse <ulysse@alenvi.io>
2023-12-14 17:39:03 +0100

    COM-3632: group  by company

--- src/core/components/courses/TrainerMissionCreationModal.vue
+++ src/core/components/courses/TrainerMissionCreationModal.vue
@@ -6,9 +6,12 @@
     <ni-select in-modal :model-value="trainerMission.program" caption="Programme" :options="programOptions"
       required-field @update:model-value="update($event, 'program')" />
     <div v-if="trainerMission.program">
-      <ni-option-group in-modal :model-value="trainerMission.courses" :options="coursesOptions" type="checkbox"
-        @update:model-value="update($event, 'courses')" :error="validations.courses.$error" caption="Formations"
-        required-field />
+      <div v-for="(companies, index) of companiesList" :key="index">
+        <div class="text-copper-500">{{ companiesName[companies] }}</div>
+        <ni-option-group in-modal :model-value="trainerMission.courses" :options="coursesOptions[companies]"
+          type="checkbox" @update:model-value="update($event, 'courses')" :error="validations.courses.$error"
+          caption="Formations" required-field />
+      </div>
       <ni-input in-modal caption="Frais de formateur" :error="validations.fee.$error" type="number" suffix="€"
         :model-value="trainerMission.fee" @blur="validations.fee.$touch" required-field
         @update:model-value="update($event, 'fee')" error-message="Valeur non valide" />
@@ -34,6 +37,7 @@ import OptionGroup from '@components/form/OptionGroup';
 import Input from '@components/form/Input';
 import Select from '@components/form/Select';
 import { composeCourseName } from '@helpers/courses';
+import { formatName, sortStrings } from '@helpers/utils';
 import { DOC_EXTENSIONS, IMAGE_EXTENSIONS } from '@data/constants';
 
 export default {
@@ -61,8 +65,34 @@ export default {
     const programOptions = computed(() => uniqBy(courses.value.map(c => c.subProgram.program), '_id')
       .map(p => ({ label: p.name, value: p._id })));
 
-    const coursesOptions = computed(() => coursesGroupedByProgram.value[trainerMission.value.program]
-      .map(c => ({ value: c._id, label: composeCourseName(c, false) })));
+    const coursesGroupedByCompany = computed(() => {
+      const sortedCourses = coursesGroupedByProgram.value[trainerMission.value.program]
+        .map(course => ({ ...course, companies: course.companies.sort((a, b) => sortStrings(a.name, b.name)) }))
+        .sort((a, b) => sortStrings(formatName(a.companies), formatName(b.companies)));
+
+      return groupBy(sortedCourses, course => course.companies.map(c => c._id));
+    });
+
+    const companiesList = computed(() => Object.keys(coursesGroupedByCompany.value));
+
+    const companiesName = computed(() => {
+      const names = {};
+      for (const companies of Object.keys(coursesGroupedByCompany.value)) {
+        names[companies] = formatName(coursesGroupedByCompany.value[companies][0].companies);
+      }
+
+      return names;
+    });
+
+    const coursesOptions = computed(() => {
+      const options = {};
+      for (const companies of Object.keys(coursesGroupedByCompany.value)) {
+        options[companies] = coursesGroupedByCompany.value[companies]
+          .map(c => ({ value: c._id, label: composeCourseName(c, false) }));
+      }
+
+      return options;
+    });
 
     const hide = () => emit('hide');
 
@@ -80,9 +110,11 @@ export default {
       DOC_EXTENSIONS,
       IMAGE_EXTENSIONS,
       // Computed
-      coursesOptions,
       programOptions,
       coursesGroupedByProgram,
+      companiesList,
+      companiesName,
+      coursesOptions,
       // Methods
       hide,
       input,
