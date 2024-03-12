3d7aced31797336a831da14c0e2405dbeaa2973b
Manon Palin <manonpalin@macbook-pro-de-manon.home>
2024-03-12 11:48:12 +0100

    COM-3668 - change labels in SurveyChart and Survey

--- src/core/components/Rating.vue
+++ src/core/components/Rating.vue
@@ -2,7 +2,7 @@
   <div class="row items-start">
     <div class="caption input-caption">
       <p :class="[{ required: requiredField }]">{{ caption }}</p>
-      <span class="q-mx-sm">(1 : {{ leftLabel }} - 5 : {{ rightLabel }})</span>
+      <span class="q-mx-sm">(1 : {{ firstLabel }} - 5 : {{ lastLabel }})</span>
     </div>
     <q-icon v-if="error" name="error_outline" color="secondary" class="col-1" />
   </div>
@@ -10,10 +10,10 @@
     <q-rating :model-value="modelValue" @update:model-value="update" :icon="icon" max="5" color="primary"
       size="lg" class="q-mx-sm">
       <template #tip-1>
-        <q-tooltip>{{ leftLabel }}</q-tooltip>
+        <q-tooltip>{{ firstLabel }}</q-tooltip>
       </template>
       <template #tip-5>
-        <q-tooltip>{{ rightLabel }}</q-tooltip>
+        <q-tooltip>{{ lastLabel }}</q-tooltip>
       </template>
     </q-rating>
   </q-field>
@@ -26,8 +26,8 @@ export default {
   name: 'Rating',
   props: {
     modelValue: { type: Number, default: 0 },
-    leftLabel: { type: String, required: true },
-    rightLabel: { type: String, required: true },
+    firstLabel: { type: String, required: true },
+    lastLabel: { type: String, required: true },
     icon: { type: [String, Array], default: 'circle' },
     caption: { type: String, default: '' },
     error: { type: Boolean, default: false },
