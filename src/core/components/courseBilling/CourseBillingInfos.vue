9fd0e99c9cd38149c772989405a12b5261dcf445
ulysse <ulysse@alenvi.io>
2024-03-22 10:11:15 +0100

    COM-3671: limit API calls

--- src/core/components/courseBilling/CourseBillingInfos.vue
+++ src/core/components/courseBilling/CourseBillingInfos.vue
@@ -504,7 +504,9 @@ export default {
     });
 
     const created = async () => {
-      if (get(company.value, '_id')) await Promise.all([refreshCourseBills(), refreshBillingRepresentativeOptions()]);
+      if (get(company.value, '_id') && hasHoldingRole.value) {
+        await Promise.all([refreshCourseBills(), refreshBillingRepresentativeOptions()]);
+      }
     };
 
     created();
