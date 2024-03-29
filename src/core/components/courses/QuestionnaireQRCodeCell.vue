f7a111c99408b0c486b6275812d96febc8953dad
Manon Palin <manonpalin@macbook-pro-de-manon.home>
2024-03-29 17:46:43 +0100

    COM-3676 : display QRCode cells

--- src/core/components/courses/QuestionnaireQRCodeCell.vue
+++ src/core/components/courses/QuestionnaireQRCodeCell.vue
@@ -1,18 +1,18 @@
 <template>
   <q-card class="questionnaire-link">
-    <text class="questionnaire-info">Répondre au questionnaire de {{ questionnaireTypeTitle }}</text>
+    <text class="questionnaire-info">Répondre au questionnaire {{ questionnaireTypeTitle }}</text>
     <div class="questionnaire-qrcode">
       <img :src="img" width="160" :alt="qrCodePlaceHolder">
     </div>
     <a class="clickable-name cursor-pointer text-link" @click="onClick">
-      Lien pour répondre au questionnaire de {{ questionnaireTypeTitle }}
+      Lien pour répondre au questionnaire {{ questionnaireTypeTitle }}
     </a>
   </q-card>
 </template>
 
 <script>
 import { toRefs, computed } from 'vue';
-import { EXPECTATIONS } from '../../data/constants';
+import { END_OF_COURSE, EXPECTATIONS } from '../../data/constants';
 
 export default {
   name: 'QuestionnaireQRCodeCell',
@@ -24,12 +24,18 @@ export default {
   setup (props, { emit }) {
     const { type } = toRefs(props);
 
-    const questionnaireTypeTitle = computed(() => (type.value === EXPECTATIONS
-      ? 'recueil des attentes'
-      : 'fin de formation'));
+    const questionnaireTypeTitle = computed(() => {
+      switch (type.value) {
+        case EXPECTATIONS:
+          return 'de recueil des attentes';
+        case END_OF_COURSE:
+          return 'de fin de formation';
+        default:
+          return 'd\'auto-positionnement';
+      }
+    });
 
-    const qrCodePlaceHolder = computed(() => `QR Code pour répondre au questionnaire
-      de ${questionnaireTypeTitle.value}`);
+    const qrCodePlaceHolder = computed(() => `QR Code pour répondre au questionnaire ${questionnaireTypeTitle.value}`);
 
     const onClick = () => emit('click');
 
