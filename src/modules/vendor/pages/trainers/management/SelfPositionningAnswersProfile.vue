58e3dbb6db5592ceab144c98115b4e8cb41d7926
Manon Palin <manonpalin@MacBook-Pro-de-Manon.local>
2024-05-23 15:51:24 +0200

    COM-3715 : display headerInfo

--- src/modules/vendor/pages/trainers/management/SelfPositionningAnswersProfile.vue
+++ src/modules/vendor/pages/trainers/management/SelfPositionningAnswersProfile.vue
@@ -1,16 +1,18 @@
 <template>
   <q-page padding class="vendor-background">
-    <ni-profile-header title="Auto-positionnement : réponses" class="q-mb-xl" />
-    {{ questionnaireAnswers.followUp.length }}
+    <ni-profile-header title="Auto-positionnement : réponses" :header-info="headerInfo" class="q-mb-xl" />
+    {{ get(questionnaireAnswers, 'followUp', []).length }}
   </q-page>
 </template>
 
 <script>
-import { toRefs, ref } from 'vue';
+import { toRefs, ref, computed } from 'vue';
+import get from 'lodash/get';
 import Questionnaires from '@api/Questionnaires';
-import { REVIEW } from '@data/constants';
+import { REVIEW, INTRA, INTRA_HOLDING } from '@data/constants';
 import { NotifyNegative } from '@components/popup/notify';
 import ProfileHeader from '@components/ProfileHeader.vue';
+import { composeCourseName } from '@helpers/courses';
 
 export default {
   name: 'SelfPositionningAnswersProfile',
@@ -23,18 +25,28 @@ export default {
   },
   setup (props) {
     const { courseId, questionnaireId } = toRefs(props);
-    const questionnaireAnswers = ref({ followUp: [], course: {} });
+    const questionnaireAnswers = ref({});
 
     const getQuestionnaireAnswers = async () => {
       try {
         const query = { course: courseId.value, action: REVIEW };
         questionnaireAnswers.value = await Questionnaires.getQuestionnaireAnswers(questionnaireId.value, query);
       } catch (e) {
-        questionnaireAnswers.value = { followUp: [], course: {} };
+        questionnaireAnswers.value = {};
         console.error(e);
         NotifyNegative('Erreur lors de la récupération des réponses au questionnaire.');
       }
     };
+
+    const headerInfo = computed(() => {
+      const { course } = questionnaireAnswers.value;
+      const infos = [{ icon: 'bookmark_border', label: get(course, 'subProgram') && composeCourseName(course) }];
+
+      if ([INTRA, INTRA_HOLDING].includes(get(course, 'type'))) {
+        infos.push({ icon: 'apartment', label: get(course, 'holding.name') || course.companies[0].name });
+      }
+      return infos;
+    });
 
     const created = async () => {
       await getQuestionnaireAnswers();
@@ -45,6 +57,9 @@ export default {
     return {
       // Data
       questionnaireAnswers,
+      get,
+      // Computed
+      headerInfo,
     };
   },
 };
