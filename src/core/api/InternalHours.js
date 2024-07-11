be15450b9fb3f1f4002e1c8f6256e8a7d2c492bf
Sophie Moustard <sophiemoustard@sophies-macbook-pro.home>
2024-07-11 17:20:01 +0200

    COM-3744 - internal hour and users

--- src/core/api/InternalHours.js
+++
@@ -1,8 +0,0 @@
-import { alenviAxios } from '@api/ressources/alenviAxios';
-
-export default {
-  async list (params) {
-    const internalHours = await alenviAxios.get(`${process.env.API_HOSTNAME}/internalhours`, { params });
-    return internalHours.data.data.internalHours;
-  },
-};
