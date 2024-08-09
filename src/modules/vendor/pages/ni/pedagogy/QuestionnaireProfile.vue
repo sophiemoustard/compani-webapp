6c66051fb257ec18d91cea2d35045b53e99e78b9
Manon Palin <manonpalin@mbp-de-manon.home>
2024-08-09 11:35:34 +0200

    COM-3701 - navigation between QuestionnaireProfile and QuestionaireAnswersProfile

--- src/modules/vendor/pages/ni/pedagogy/QuestionnaireProfile.vue
+++ src/modules/vendor/pages/ni/pedagogy/QuestionnaireProfile.vue
@@ -1,7 +1,11 @@
 <template>
   <q-page padding class="vendor-background column no-wrap">
     <template v-if="questionnaire">
-      <ni-profile-header :title="questionnaireName" :header-info="headerInfo" />
+      <ni-profile-header :title="questionnaireName" :header-info="headerInfo">
+        <template #title>
+          <ni-primary-button label="Voir les réponses" @click="goToQuestionnaireProfileAnswers" />
+        </template>
+      </ni-profile-header>
       <profile-edition :profile-id="questionnaireId" class="edition" />
     </template>
   </q-page>
@@ -13,8 +17,10 @@ import { ref, computed, watch, onBeforeUnmount, toRefs } from 'vue';
 import { useStore } from 'vuex';
 import get from 'lodash/get';
 import ProfileHeader from '@components/ProfileHeader';
-import { DRAFT, QUESTIONNAIRE_TYPES } from '@data/constants';
+import { DRAFT, QUESTIONNAIRE_TYPES, SELF_POSITIONNING } from '@data/constants';
+import PrimaryButton from '@components/PrimaryButton';
 import ProfileEdition from 'src/modules/vendor/components/questionnaires/ProfileEdition';
+import { useRouter } from 'vue-router';
 
 const metaInfo = { title: 'Fiche questionnaire' };
 
@@ -25,6 +31,7 @@ export default {
   },
   components: {
     'ni-profile-header': ProfileHeader,
+    'ni-primary-button': PrimaryButton,
     'profile-edition': ProfileEdition,
   },
   mixins: [createMetaMixin(metaInfo)],
@@ -32,7 +39,10 @@ export default {
     const { questionnaireId } = toRefs(props);
     const questionnaireName = ref('');
 
+    const $router = useRouter();
+
     const $store = useStore();
+
     const questionnaire = computed(() => $store.state.questionnaire.questionnaire);
     const headerInfo = computed(() => {
       const infos = [{ icon: 'bookmark_border', label: QUESTIONNAIRE_TYPES[questionnaire.value.type] }];
@@ -51,6 +61,14 @@ export default {
       await $store.dispatch('questionnaire/fetchQuestionnaire', { questionnaireId: questionnaireId.value });
     };
 
+    const goToQuestionnaireProfileAnswers = () => {
+      const query = questionnaire.value.type === SELF_POSITIONNING
+        ? { questionnaireType: questionnaire.value.type, programId: questionnaire.value.program }
+        : { questionnaireType: questionnaire.value.type };
+
+      $router.push({ name: 'ni pedagogy questionnaire answers', query });
+    };
+
     const created = async () => {
       await refreshQuestionnaire();
       refreshProgramName();
@@ -66,6 +84,8 @@ export default {
       // Computed
       questionnaire,
       headerInfo,
+      // Methods
+      goToQuestionnaireProfileAnswers,
     };
   },
 };
