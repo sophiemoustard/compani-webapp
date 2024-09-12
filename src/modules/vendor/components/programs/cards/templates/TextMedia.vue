9bb67b388cbd8f496420bf42d37471e661ddb5f1
Aude <aude@compani.fr>
2024-09-12 12:32:08 +0200

    COM-3785: Migrate TextMedia TemplateMixin to CardTemplate

--- src/modules/vendor/components/programs/cards/templates/TextMedia.vue
+++ src/modules/vendor/components/programs/cards/templates/TextMedia.vue
@@ -2,8 +2,9 @@
   <div>
     <ni-input caption="Texte" v-model="card.text" required-field @focus="saveTmp('text')"
       @blur="updateCard('text')" :error="v$.card.text.$error" type="textarea" :disable="disableEdition" />
-    <ni-option-group v-model="card.media.type" :options="extensionOptions" inline :error="v$.card.media.type.$error"
-      type="radio" :disable="isUploading || !!card.media.publicId" @update:model-value="updateCard('media.type')" />
+    <ni-option-group v-model="card.media.type" :options="UPLOAD_EXTENSION_OPTIONS" inline
+      :error="v$.card.media.type.$error" type="radio" :disable="isUploading || !!card.media.publicId"
+      @update:model-value="updateCard('media.type')" />
     <ni-file-uploader class="file-uploader" caption="Média" path="media" :entity="card" name="media"
       @uploaded="mediaUploaded()" @delete="validateMediaDeletion()" :error="v$.card.media.$error"
       :extensions="extensions" :additional-value="mediaFileName" required-field :disable="disableEdition"
@@ -12,22 +13,27 @@
 </template>
 
 <script>
-import { computed } from 'vue';
+import { computed, toRefs } from 'vue';
 import { useStore } from 'vuex';
+import { useCardTemplate } from 'src/modules/vendor/composables/CardTemplate';
 import useVuelidate from '@vuelidate/core';
 import { required } from '@vuelidate/validators';
 import Input from '@components/form/Input';
 import FileUploader from '@components/form/FileUploader';
-import { templateMixin } from 'src/modules/vendor/mixins/templateMixin';
 import OptionGroup from '@components/form/OptionGroup';
+import { UPLOAD_EXTENSION_OPTIONS } from '@data/constants';
 
 export default {
   name: 'TextMedia',
   props: {
     disableEdition: { type: Boolean, default: false },
     cardParent: { type: Object, default: () => ({}) },
+    isUploading: { type: Boolean, default: false },
   },
-  setup () {
+  emits: ['refresh', 'card-parent'],
+  setup (props, { emit }) {
+    const { cardParent } = toRefs(props);
+
     const $store = useStore();
 
     const card = computed(() => $store.state.card.card);
@@ -41,11 +47,39 @@ export default {
 
     const v$ = useVuelidate(rules, { card });
 
+    const refreshCard = () => { emit('refresh'); };
+
+    const {
+      saveTmp,
+      updateCard,
+      mediaUploaded,
+      validateMediaDeletion,
+      mediaFileName,
+      mediaUploadUrl,
+      extensions,
+      maxFileSize,
+      start,
+      finish,
+    } = useCardTemplate(card, v$, refreshCard, cardParent);
+
     return {
       // Validation
       v$,
+      // Data
+      UPLOAD_EXTENSION_OPTIONS,
       // Computed
       card,
+      mediaFileName,
+      mediaUploadUrl,
+      extensions,
+      maxFileSize,
+      // Methods
+      saveTmp,
+      updateCard,
+      mediaUploaded,
+      validateMediaDeletion,
+      start,
+      finish,
     };
   },
   components: {
@@ -53,7 +87,6 @@ export default {
     'ni-file-uploader': FileUploader,
     'ni-option-group': OptionGroup,
   },
-  mixins: [templateMixin],
 };
 </script>
 
--- src/modules/vendor/composables/CardTemplate.js
+++ src/modules/vendor/composables/CardTemplate.js
@@ -12,11 +12,18 @@ import {
   SINGLE_CHOICE_QUESTION,
   QUESTION_MAX_LENGTH,
   REQUIRED_LABEL,
+  UPLOAD_IMAGE,
+  UPLOAD_VIDEO,
+  UPLOAD_AUDIO,
+  AUDIO_EXTENSIONS,
+  IMAGE_EXTENSIONS,
+  VIDEO_EXTENSIONS,
 } from '@data/constants';
 
-export const useCardTemplate = (card, v$, refreshCard) => {
+export const useCardTemplate = (card, v$, refreshCard, cardParent) => {
   const tmpInput = ref('');
   const $q = useQuasar();
+  const isUploading = ref(false);
 
   const questionErrorMsg = computed(() => {
     if (get(v$.value, 'card.question.required.$response') === false) return REQUIRED_LABEL;
@@ -126,11 +133,92 @@ export const useCardTemplate = (card, v$, refreshCard) => {
     }
   };
 
+  const mediaUploaded = async () => {
+    try {
+      await refreshCard();
+      NotifyPositive('Média envoyé');
+    } catch (e) {
+      NotifyNegative('Erreur lors de l\'envoi du média');
+    }
+  };
+
+  const deleteMedia = async () => {
+    try {
+      if (get(card.value, 'media')) {
+        await Cards.deleteMedia(card.value._id);
+
+        await refreshCard();
+        NotifyPositive('Média supprimé');
+      } else NotifyNegative('Erreur lors de la suppression du média.');
+    } catch (e) {
+      console.error(e);
+      NotifyNegative('Erreur lors de la suppression du média.');
+    }
+  };
+
+  const validateMediaDeletion = (path) => {
+    $q.dialog({
+      title: 'Confirmation',
+      message: 'Êtes-vous sûr(e) de vouloir supprimer ce média&nbsp;?',
+      html: true,
+      ok: true,
+      cancel: 'Annuler',
+    }).onOk(() => deleteMedia(path))
+      .onCancel(() => NotifyPositive('Suppression annulée.'));
+  };
+
+  const mediaFileName = computed(() => {
+    if (card.value && card.value.title) return card.value.title.replace(/ /g, '_');
+
+    return cardParent.value.name.replace(/ /g, '_');
+  });
+
+  const mediaUploadUrl = computed(() => `${process.env.API_HOSTNAME}/cards/${card.value._id}/upload`);
+
+  const extensions = computed(() => {
+    switch (card.value.media.type) {
+      case UPLOAD_VIDEO:
+        return VIDEO_EXTENSIONS;
+      case UPLOAD_AUDIO:
+        return AUDIO_EXTENSIONS;
+      case UPLOAD_IMAGE:
+        return IMAGE_EXTENSIONS;
+      default:
+        return '';
+    }
+  });
+
+  const maxFileSize = computed(() => {
+    switch (card.value.media.type) {
+      case UPLOAD_IMAGE:
+        return 300 * 1000;
+      case UPLOAD_AUDIO:
+        return 5 * 1000 * 1000;
+      case UPLOAD_VIDEO:
+        return 25 * 1000 * 1000;
+      default:
+        return 0;
+    }
+  });
+
+  const start = () => {
+    isUploading.value = true;
+  };
+
+  const finish = () => {
+    isUploading.value = false;
+  };
+
   return {
     // Data
     tmpInput,
+    isUploading,
     // Computed
     questionErrorMsg,
+    mediaFileName,
+    mediaUploadUrl,
+    extensions,
+    maxFileSize,
     // Methods
     saveTmp,
     updateCard,
@@ -138,5 +226,10 @@ export const useCardTemplate = (card, v$, refreshCard) => {
     updateTextAnswer,
     addAnswer,
     validateAnswerDeletion,
+    mediaUploaded,
+    deleteMedia,
+    validateMediaDeletion,
+    start,
+    finish,
   };
 };
