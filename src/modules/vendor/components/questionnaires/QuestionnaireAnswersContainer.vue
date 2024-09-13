23020a00da53e55065205cfc25f0076c20b08013
Aude <aude@compani.fr>
2024-09-13 17:18:18 +0200

    COM-3765: add files QuestionnaireAnswersContainer + QuestionnaireAnswer composable

---
+++ src/modules/vendor/components/questionnaires/QuestionnaireAnswersContainer.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <span class="section-title">{{ title }}</span>
+    <q-card
+      v-for="(card, cardIndex) of cards" :key="cardIndex" flat class="q-mb-sm">
+      <component :is="getChartComponent(card.template)" :card="card" />
+    </q-card>
+  </div>
+</template>
+
+<script>
+import { useQuestionnaireAnswers } from 'src/modules/vendor/composables/QuestionnaireAnswers';
+
+export default {
+  name: 'QuestionnaireAnswersContainer',
+  props: {
+    title: { type: String, default: '' },
+    cards: { type: Array, required: true },
+  },
+  setup () {
+    const { getChartComponent } = useQuestionnaireAnswers();
+
+    return {
+      // Methods
+      getChartComponent,
+    };
+  },
+};
+</script>
+
+<style lang="sass" scoped>
+.section-title
+  font-size: 24px
+  font-weight: bold
+  color: $copper-grey-700
+  margin: 12px
+</style>
