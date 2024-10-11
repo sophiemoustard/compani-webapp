9fd67c22c07d257802bf68b8bbef0bafd37e8c87
Manon Palin <manonpalin@mbp-de-manon.home>
2024-10-11 12:47:33 +0200

    COM-3799 : remove useless div

--- src/core/components/courses/ProfileOrganization.vue
+++ src/core/components/courses/ProfileOrganization.vue
@@ -31,12 +31,10 @@
           @open-modal="openSalesRepresentativeModal" clearable />
       </div>
       <p class="text-weight-bold table-title q-mt-xl">Intervenants</p>
-      <div class="interlocutor-container" v-if="get(course, 'trainers', []).some(t => t._id)">
-        <div v-for="trainer in course.trainers" :key="trainer._id">
-          <interlocutor-cell :interlocutor="trainer" caption="Intervenant" :contact="course.contact"
-            :can-update="canUpdateInterlocutor" label="Ajouter un intervenant" :disable="isArchived"
-            clearable is-trainer />
-        </div>
+      <div v-if="get(course, 'trainers', []).some(t => t._id)" class="interlocutor-container">
+        <interlocutor-cell v-for="trainer in course.trainers" :key="trainer._id" :interlocutor="trainer"
+          caption="Intervenant" :contact="course.contact" :can-update="canUpdateInterlocutor"
+          label="Ajouter un intervenant" :disable="isArchived" clearable is-trainer />
       </div>
       <p v-else class="text-italic q-mb-md ">Aucun intervenant n'est défini pour cette formation.</p>
       <ni-secondary-button v-if="canUpdateInterlocutor" class="q-my-lg" label="Ajouter un intervenant"
