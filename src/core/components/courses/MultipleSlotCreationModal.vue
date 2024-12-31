37a88da411f5e9c32b529a573e3e0079f447e14d
Aude <aude@compani.fr>
2024-12-31 09:29:55 +0100

    COM-3872: add MultipleSlotCreationModal file

---
+++ src/core/components/courses/MultipleSlotCreationModal.vue
@@ -0,0 +1,46 @@
+<template>
+  <ni-modal :model-value="modelValue" @hide="hide">
+    <template #title>
+      Ajouter des créneaux
+    </template>
+    <ni-input in-modal caption="Nombre de créneaux à ajouter" type="number" required
+      @update:model-value="update($event)" />
+    <template #footer>
+      <ni-button class="bg-primary full-width modal-btn" label="Ajouter les créneaux" icon="add" color="white"
+        :loading="loading" @click="submit" />
+    </template>
+  </ni-modal>
+</template>
+
+<script>
+import Modal from '@components/modal/Modal';
+import Input from '@components/form/Input';
+
+export default {
+  name: 'MultipleSlotCreationModal',
+  props: {
+    modelValue: { type: Boolean, default: false },
+    validations: { type: Object, default: () => ({}) },
+    loading: { type: Boolean, default: false },
+  },
+  components: {
+    'ni-modal': Modal,
+    'ni-input': Input,
+  },
+  emits: ['hide', 'submit', 'update:slots-quantity'],
+  setup (_, emit) {
+    const hide = () => emit('hide');
+
+    const submit = () => emit('submit');
+
+    const update = event => emit('update:slots-quantity', event);
+
+    return {
+      // Methods
+      hide,
+      submit,
+      update,
+    };
+  },
+};
+</script>
