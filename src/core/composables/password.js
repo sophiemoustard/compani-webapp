0fe0fcb0101fc93fec5e4a2f019653268670133b
Aude <aude@compani.fr>
2025-01-30 09:24:31 +0100

    COM-3887: fix review

--- src/core/composables/password.js
+++ src/core/composables/password.js
@@ -1,5 +1,5 @@
 import { ref } from 'vue';
-import get from 'lodash/get';
+// import get from 'lodash/get';
 import { minLength } from '@vuelidate/validators';
 import { REQUIRED_LABEL } from '@data/constants';
 
@@ -7,16 +7,14 @@ export const usePassword = () => {
   const passwordValidation = ref({ minLength: minLength(6) });
 
   const passwordError = (validationObj) => {
-    if (get(validationObj, 'required.$response') === false) return REQUIRED_LABEL;
-    if (get(validationObj, 'minLength.$response') === false) {
-      return 'Le mot de passe doit contenir au minimum 6 caractères.';
-    }
+    if (validationObj.required.$response === false) return REQUIRED_LABEL;
+    if (validationObj.minLength.$response === false) return 'Le mot de passe doit contenir au minimum 6 caractères.';
     return '';
   };
 
   const passwordConfirmError = (validationObj) => {
-    if (get(validationObj, 'required.$response') === false) return REQUIRED_LABEL;
-    if (get(validationObj, 'sameAs.$response') === false) return 'Le mot de passe doit être identique';
+    if (validationObj.required.$response === false) return REQUIRED_LABEL;
+    if (validationObj.sameAs.$response === false) return 'Le mot de passe doit être identique.';
     return 'Mot de passe invalide';
   };
 
